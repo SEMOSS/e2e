9b72e874a0ef95cabca4d2dd615d4f22d02e41de
Jeff Vitunac <jvitunac@gmail.com>
2025-02-05 11:12:09 -0500

    test shell scripts

---
+++ testNRM.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker compose up --build --exit-code-from test
--- tester.sh
+++ tester.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker compose up --build --exit-code-from test && docker compose rm -f semoss && docker container rm -f e2e-semoss-1
+docker compose up --build --exit-code-from test ; docker compose rm -f semoss
