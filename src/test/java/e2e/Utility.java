f7ad044366ebc31c4bfc2623e8567e18c31ec8bb
Jeff Vitunac <jvitunac@gmail.com>
2025-02-27 11:11:02 -0500

    comment out old e2e stuff

--- src/test/java/e2e/Utility.java
+++ src/test/java/e2e/Utility.java
@@ -3,22 +3,22 @@ package e2e;
 public class Utility {
 	
 	
-	private static boolean registered = false;
-	
-	public static boolean getRegistered() {
-		if (!registered) {
-			registered = true;
-			return false;
-		}
-		return registered;
-	}
-	
-	public static String getUrl(String ending) {
-		return E2ETests.getUrlPrefix() + ending;
-	}
-
-	protected static String getApi(String ending) {
-		return E2ETests.getApiPrefix() + ending;
-	}
+//	private static boolean registered = false;
+//	
+//	public static boolean getRegistered() {
+//		if (!registered) {
+//			registered = true;
+//			return false;
+//		}
+//		return registered;
+//	}
+//	
+//	public static String getUrl(String ending) {
+//		return E2ETests.getUrlPrefix() + ending;
+//	}
+//
+//	protected static String getApi(String ending) {
+//		return E2ETests.getApiPrefix() + ending;
+//	}
 
 }
--- src/test/java/e2e/admin/UserManagementTests.java
+++ src/test/java/e2e/admin/UserManagementTests.java
@@ -1,73 +1,61 @@
 package e2e.admin;
 
-import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
-
-import java.util.regex.Pattern;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import com.microsoft.playwright.Locator;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.options.AriaRole;
-
 import e2e.E2EAdminTests;
-import e2e.LoginUtils;
 
 public class UserManagementTests extends E2EAdminTests {
 
-	private static final Logger LOGGER = LogManager.getLogger(UserManagementTests.class);
-
-	//@Test
-	void createAndDeleteUser() {
-		page.getByLabel("Navigate to app library").click();
-		page.getByTestId("Function-icon").click();
-		page.getByTestId("Model-icon").click();
-		page.getByTestId("Database-icon").click();
-		page.getByTestId("Vector-icon").click();
-		page.getByTestId("Storage-icon").click();
-		page.getByLabel("Navigate to settings").click();
-		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Admin Off")).click();
-		page.getByText("Add new members, reset").click();
-		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add User")).click();
-		page.getByLabel("Name").first().click();
-		page.getByLabel("Name").first().fill("Joe Shmoe");
-		page.locator("input[type=\"email\"]").fill("jshmoe@test.com");
-		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^Phone Number$")))
-				.locator("input").fill("5555555555");
-		page.getByLabel("Type").click();
-		page.getByRole(AriaRole.OPTION, new Page.GetByRoleOptions().setName("Native")).click();
-		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^User Id$"))).click();
-		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^User Id$")))
-				.locator("input").fill("jshmoe");
-		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^Username$"))).click();
-		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^Username$")))
-				.locator("input").fill("jshmoe");
-		page.getByLabel("Password").click();
-		page.getByLabel("Password").fill("TestTest9*");
-		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Save")).click();
-		page.getByText("Joe Shmoe").click();
-		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^SEMOSS$")))
-				.getByRole(AriaRole.BUTTON).click();
-		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Logout")).click();
-
-		LoginUtils.login(page, context, "jshmoe", "TestTest9*");
-		
-		page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("SEMOSS")).click();
-		page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Apps")).click();
-		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^SEMOSS$")))
-				.getByRole(AriaRole.BUTTON).click();
-		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Logout")).click();
-
-		LoginUtils.login(page, context, "user1", "TestTest8*");
-
-		page.getByLabel("Navigate to settings").click();
-		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Admin Off")).click();
-		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^Member Settings$"))).first()
-				.click();
-		page.getByRole(AriaRole.ROW, new Page.GetByRoleOptions().setName("JS Joe Shmoe User ID: jshmoe"))
-				.getByRole(AriaRole.BUTTON).nth(1).click();
-		assertThat(page.locator("tfoot")).matchesAriaSnapshot("- paragraph: 1–1 of 1");
-	}
+//	private static final Logger LOGGER = LogManager.getLogger(UserManagementTests.class);
+//
+//	//@Test
+//	void createAndDeleteUser() {
+//		page.getByLabel("Navigate to app library").click();
+//		page.getByTestId("Function-icon").click();
+//		page.getByTestId("Model-icon").click();
+//		page.getByTestId("Database-icon").click();
+//		page.getByTestId("Vector-icon").click();
+//		page.getByTestId("Storage-icon").click();
+//		page.getByLabel("Navigate to settings").click();
+//		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Admin Off")).click();
+//		page.getByText("Add new members, reset").click();
+//		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add User")).click();
+//		page.getByLabel("Name").first().click();
+//		page.getByLabel("Name").first().fill("Joe Shmoe");
+//		page.locator("input[type=\"email\"]").fill("jshmoe@test.com");
+//		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^Phone Number$")))
+//				.locator("input").fill("5555555555");
+//		page.getByLabel("Type").click();
+//		page.getByRole(AriaRole.OPTION, new Page.GetByRoleOptions().setName("Native")).click();
+//		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^User Id$"))).click();
+//		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^User Id$")))
+//				.locator("input").fill("jshmoe");
+//		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^Username$"))).click();
+//		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^Username$")))
+//				.locator("input").fill("jshmoe");
+//		page.getByLabel("Password").click();
+//		page.getByLabel("Password").fill("TestTest9*");
+//		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Save")).click();
+//		page.getByText("Joe Shmoe").click();
+//		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^SEMOSS$")))
+//				.getByRole(AriaRole.BUTTON).click();
+//		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Logout")).click();
+//
+//		LoginUtils.login(page, context, "jshmoe", "TestTest9*");
+//		
+//		page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("SEMOSS")).click();
+//		page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Apps")).click();
+//		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^SEMOSS$")))
+//				.getByRole(AriaRole.BUTTON).click();
+//		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Logout")).click();
+//
+//		LoginUtils.login(page, context, "user1", "TestTest8*");
+//
+//		page.getByLabel("Navigate to settings").click();
+//		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Admin Off")).click();
+//		page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^Member Settings$"))).first()
+//				.click();
+//		page.getByRole(AriaRole.ROW, new Page.GetByRoleOptions().setName("JS Joe Shmoe User ID: jshmoe"))
+//				.getByRole(AriaRole.BUTTON).nth(1).click();
+//		assertThat(page.locator("tfoot")).matchesAriaSnapshot("- paragraph: 1–1 of 1");
+//	}
 
 }
