4ca24b84ceea9be69bf3ba47e3f9cfa9bf05af94
Hanumant2828 <hanumantc2007@gmail.com>
2025-03-05 04:52:10 +0530

    Issue-28 view model settings (#38)
    * Issue-28 view model settings
    
    * removed debugger step
    
    ---------
    
    Co-authored-by: hchoramale <hchoramale@deloitte.com>
    Co-authored-by: roxy <rocioram94@gmail.com>

---
+++ src/test/java/aicore/steps/ModelSettingsSteps.java
@@ -0,0 +1,140 @@
+package aicore.steps;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.assertj.core.api.SoftAssertions;
+
+import aicore.base.AICoreTestManager;
+import aicore.hooks.SoftAssertionHooks;
+import aicore.pages.AddModelToCatalogPage;
+import aicore.utils.CommonUtils;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ModelSettingsSteps {
+	private AddModelToCatalogPage modelPage;
+	private String timestamp;
+	private SoftAssertions softAssert;
+
+	public ModelSettingsSteps() {
+		timestamp = CommonUtils.getTimeStampName();
+		modelPage = new AddModelToCatalogPage(AICoreTestManager.getPage(), timestamp);
+		softAssert = SoftAssertionHooks.getSoftAssertions();
+	}
+
+	@When("User click on Setting tab")
+	public void user_click_on_setting_tab() {
+		modelPage.clickOnSettingsTab();
+	}
+
+	@Then("User can see {string} section")
+	public void user_can_see_section(String sectionName) {
+		boolean isSectionVisible = false;
+		switch (sectionName) {
+		case "Make Public":
+			isSectionVisible = modelPage.verifyMakePublicSectionIsVisible(sectionName);
+			break;
+		case "Make Discoverable":
+			isSectionVisible = modelPage.verifyMakeDiscoverableSectionIsVisible(sectionName);
+			break;
+		case "Delete":
+			isSectionVisible = modelPage.verifyDeleteSectionIsVisible(sectionName);
+			break;
+		case "Pending Requests":
+			isSectionVisible = modelPage.verifyPendingRequestsSectionIsVisible();
+			break;
+		case "Members":
+			isSectionVisible = modelPage.verifyMembersSectionIsVisible();
+			break;
+		default:
+			System.out.println("Invalid section name: " + sectionName);
+		}
+		softAssert.assertThat(isSectionVisible).isTrue();
+	}
+
+	@And("User can see text message in {string} section as {string}")
+	public void user_can_see_text_message_in_section_as(String sectionName, String textMessage) {
+		String actualTextMessage = null;
+		switch (sectionName) {
+		case "Make Public":
+			actualTextMessage = modelPage.verifyMakePublicSectionTextMessage();
+			break;
+		case "Make Discoverable":
+			actualTextMessage = modelPage.verifyMakeDiscoverableSectionTextMessage();
+			break;
+		case "Delete":
+			actualTextMessage = modelPage.verifyDeleteSectionTextMessage();
+			break;
+		default:
+			System.out.println("Invalid section name: " + sectionName);
+		}
+		softAssert.assertThat(actualTextMessage).isEqualTo(textMessage);
+	}
+
+	@And("User can see toggle button in {string} section")
+	public void user_can_see_toggle_button_in_section(String sectionName) {
+		boolean isToggleButtonVisible = false;
+		switch (sectionName) {
+		case "Make Public":
+			isToggleButtonVisible = modelPage.verifyMakePublicToggleButtonIsVisible();
+			break;
+		case "Make Discoverable":
+			isToggleButtonVisible = modelPage.verifyMakeDiscoverableToggleButtonIsVisible();
+			break;
+		default:
+			System.out.println("Invalid section name: " + sectionName);
+		}
+		softAssert.assertThat(isToggleButtonVisible).isTrue();
+	}
+
+	@And("User can see Delete button in {string} section")
+	public void user_can_see_delete_button_in_section(String sectionName) {
+		boolean isDeleteButtonVisible = false;
+		switch (sectionName) {
+		case "Delete":
+			isDeleteButtonVisible = modelPage.verifyDeleteButtonIsVisible();
+			break;
+		default:
+			System.out.println("Invalid section name: " + sectionName);
+		}
+		softAssert.assertThat(isDeleteButtonVisible).isTrue();
+	}
+
+	@And("User can see {string} section contains {string} or {string} text with count")
+	public void user_can_see_section_contains_or_text_wit_count(String sectionName, String singleRequesttextMessage,
+			String multipleRequesttextMessage) {
+		String pendingRequestText = modelPage.verifyPendingRequestsSectionTextMessage();
+		String extractedText = pendingRequestText.replaceAll("\\d+", "").trim();
+		boolean isValidText = extractedText.equals(singleRequesttextMessage)
+				|| extractedText.equals(multipleRequesttextMessage);
+		softAssert.assertThat(isValidText).isTrue();
+	}
+
+	@And("User can see {string} textbox")
+	public void user_can_see_textbox(String searchMembersTextBox) {
+		boolean isSearchMembersSearchBoxVisible = modelPage.verifySearchMembersSearchBoxIsVisible();
+		softAssert.assertThat(isSearchMembersSearchBoxVisible).isTrue();
+	}
+
+	@And("User can see {string} button")
+	public void user_can_see_button(String addMembersButton) {
+		boolean isAddMembersButtonVisible = modelPage.verifyAddMembersButtonIsVisible();
+		softAssert.assertThat(isAddMembersButtonVisible).isTrue();
+	}
+
+	@And("User can see {string} selection dropdown")
+	public void user_can_see_selection_dropdown(String rowsPerPageDropdown) {
+		boolean isRowsPerPageDropdownVisible = modelPage.verifyRowsPerPageDropdownIsVisible();
+		softAssert.assertThat(isRowsPerPageDropdownVisible).isTrue();
+	}
+
+	@And("User can see {string} dropdown has options {string}")
+	public void user_can_see_dropdown_has_options(String dropdownName, String expectedOptions) {
+		List<String> actualAllOptions = modelPage.verifyRowsPerPageDropdownOptions();
+		List<String> expectedOptionList = Arrays.asList(expectedOptions.split(", "));
+		softAssert.assertThat(actualAllOptions).isEqualTo(expectedOptionList);
+
+	}
+}
---
+++ src/test/resources/Features/ModelSettings.feature
@@ -0,0 +1,31 @@
+Feature: Model settings
+  I want to use this feature file for all the scenarios related to Model settings
+
+  Background: Create a model
+    Given User navigated to Open Model
+    And User clicks on Add Model
+    And User selects 'GPT-3.5'
+    And User enters Catalog name as 'Catalog'
+    And User enters open AI Key as 'Test@1234'
+    And User enters var name as 'Variable1'
+    And User clicks on Create Model button
+    Then User can see a toast message as 'Successfully added LLM to catalog'
+
+  Scenario: Validate Model Settings page
+    When User click on Setting tab
+    Then User can see 'Make Public' section
+    And User can see text message in 'Make Public' section as 'Show Model to all users and automatically give them read-only access. Users can request elevated access.'
+    And User can see toggle button in 'Make Public' section
+    And User can see 'Make Discoverable' section
+    And User can see text message in 'Make Discoverable' section as 'Allow users that do not currently have access to the Model to discover the Model, view Model details, and request access.'
+    And User can see toggle button in 'Make Discoverable' section
+    And User can see 'Delete' section
+    And User can see text message in 'Delete' section as 'Delete Model from catalog.'
+    And User can see Delete button in 'Delete' section
+    And User can see 'Pending Requests' section
+    And User can see 'Pending Requests' section contains 'pending request' or 'pending requests' text with count
+    And User can see 'Members' section
+    And User can see 'Search Members' textbox
+    And User can see 'Add Members' button
+    And User can see 'Rows per page' selection dropdown
+    And User can see 'Rows per page' dropdown has options "5, 10, 20"
