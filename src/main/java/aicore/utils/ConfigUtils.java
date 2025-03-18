1b824e9e77d001a8915e2338493de8d6dda9203a
Jeff Vitunac <jvitunac@gmail.com>
2025-03-18 11:11:35 -0400

    test: adding logging

--- src/main/java/aicore/utils/ConfigUtils.java
+++ src/main/java/aicore/utils/ConfigUtils.java
@@ -1,11 +1,18 @@
 package aicore.utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public final class ConfigUtils {
+
+	private static final Logger LOGGER = LogManager.getLogger(ConfigUtils.class);
 
 	private static Properties p;
 
@@ -14,12 +21,14 @@ public final class ConfigUtils {
 		FileInputStream f;
 		try {
 			//TODO: To be fixed for Continuous test Pipeline!
-			f = new FileInputStream("./src/main/resources/config.properties");
+			LOGGER.info("Loading properties from file");
+			Path pathToProps = Paths.get("src", "main", "resources", "config.properties");
+			f = new FileInputStream(pathToProps.toFile());
 			p.load(f);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+			LOGGER.info("Loaded properties from file");
+		} catch (Exception e) {
+			LOGGER.error("Could not load properties", e);
+			throw new RuntimeException(e);
 		}
 	}
 
@@ -27,8 +36,11 @@ public final class ConfigUtils {
 	}
 
 	public static String getValue(String key) {
+		LOGGER.info("Getting value for key: {}", key);
 		String value = p.getProperty(key);
+		LOGGER.info("KEY: {} VALUE: {}", key, value);
 		if(value==null || value.trim().length()==0) {
+			LOGGER.error("Value of key is empty: {}", key);
 			System.err.println("Property '"+key+"' is undefined in config.properties!");
 		}
 		return value;
