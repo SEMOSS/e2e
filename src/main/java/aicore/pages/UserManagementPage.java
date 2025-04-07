4f40b3029029fd121efa42463d53d1be9245f506
Singh <surajprasingh@deloitte.com>
2025-04-07 19:43:04 +0530

    issue 46 code changes

--- src/main/java/aicore/pages/UserManagementPage.java
+++ src/main/java/aicore/pages/UserManagementPage.java
@@ -10,15 +10,26 @@ public class UserManagementPage {
 	private static final String ADMIN_ON_BUTTON_XPATH = "//span[text()='Admin on']";
 	private static final String ADD_MEMBER_XPATH = "[data-testid='AddIcon']";
 	private static final String ADD_MEMBER_TYPE_XPATH = "//label[text()='Type']/parent::div";
-	private static final String ADD_MEMBER_TYPE_NATIVE_XPATH = "//li[text()='Native']";
+	private static final String ADD_MEMBER_TYPE_NATIVE_XPATH = "//li[text()='NATIVE']";
 	private static final String ADD_MEMBER_USERID_XPATH = "//label[text()='User Id']";
-	private static final String ADD_MEMBER_USERNAME_XPATH = "//label[text()='Username']/following-sibling::div/input";
 	private static final String ADD_MEMBER_NAME_XPATH = "//label[text()='Name']/following-sibling::div/input";
 	private static final String ADD_MEMBER_EMAIL_XPATH = "//label[text()='Email']/following-sibling::div/input";
 	private static final String ADD_MEMBER_PHONE_NUMBER_XPATH = "//label[text()='Phone Number']/following-sibling::div/input";
 	private static final String ADD_MEMBER_EXTENSION_XPATH = "//label[text()='Extension']/following-sibling::div/input";
 	private static final String ADD_MEMBER_TYPE_SAVE_XPATH = "//span[text()='Save']";
 	private static final String ADD_MEMBER_TOAST_MESSAGE_XPATH = "//div[text()='Successfully added user']";
+	private static final String EDIT_ICON_XPATH = "[data-testid='EditIcon']";
+	private static final String MODEL_DROPDOWN_XPATH = "//div[text()='None']";
+	private static final String TOKEN_VALUE_XPATH = "//li[text()='{dropdown_value}']";
+	private static final String MAX_TOKEN_VALUE_XPATH = "//label[text()='Max Tokens']";
+	private static final String FREQUENCY_DROPDOWN_XPATH = "//label[text()='Frequency']//parent::div//div";
+	private static final String WEEKELY_VALUE_XPATH = "//li[text()='{dropdown_option}']";
+	private static final String MODEL_LIMIT_XPATH = "//td[text()='{limitValue}']";
+	private static final String SEARCH_BUTTON_XPATH = "[placeholder=\"Search Users\"]";
+	private static final String SELECT_ALL_BUTTON_XPATH = "//label[@class=\"MuiFormControlLabel-root MuiFormControlLabel-labelPlacementEnd css-1o4vkg1-MuiFormControlLabel-root\"]/parent::th//input";
+	private static final String DELETE_MEMBER_TOAST_MESSAGE_XPATH = "//div[text()='Successfully deleted users']";
+	private static final String DELETE_SELECTED_BUTTON_XPATH = "//span[text()='Delete Selected']";
+	private static final String SEARCH_ICON_XPATH = "[data-testid=\"SearchIcon\"]";
 
 	public UserManagementPage(Page page) {
 		this.page = page;
@@ -71,11 +82,6 @@ public class UserManagementPage {
 		page.locator(ADD_MEMBER_USERID_XPATH).fill(UserId);
 	}
 
-	public void fillUsername(String Username) throws InterruptedException {
-		page.locator(ADD_MEMBER_USERNAME_XPATH).isVisible();
-		page.locator(ADD_MEMBER_USERNAME_XPATH).fill(Username);
-	}
-
 	public void fillName(String Name) throws InterruptedException {
 		page.locator(ADD_MEMBER_NAME_XPATH).isVisible();
 		page.locator(ADD_MEMBER_NAME_XPATH).fill(Name);
@@ -102,6 +108,66 @@ public class UserManagementPage {
 
 	public String userCreationToastMessage() {
 		String toastMessage = page.textContent(ADD_MEMBER_TOAST_MESSAGE_XPATH).trim();
+		return toastMessage;
+	}
+
+	public void clickOnEditUser() {
+		page.locator(EDIT_ICON_XPATH).first().isVisible();
+		page.locator(EDIT_ICON_XPATH).first().click();
+	}
+
+	public void clickModelUserDropdown() {
+		page.locator(MODEL_DROPDOWN_XPATH).isVisible();
+		page.locator(MODEL_DROPDOWN_XPATH).click();
+	}
+
+	public void selectTokenValueDropdown(String dropdownOption) {
+		page.locator(TOKEN_VALUE_XPATH.replace("{dropdown_value}", dropdownOption)).isVisible();
+		page.locator(TOKEN_VALUE_XPATH.replace("{dropdown_value}", dropdownOption)).click();
+	}
+
+	public void fillMaxTokensValue(String value) {
+		page.locator(MAX_TOKEN_VALUE_XPATH).isVisible();
+		page.locator(MAX_TOKEN_VALUE_XPATH).fill(value);
+	}
+
+	public void clickFrequencyDropdown() {
+		page.locator(FREQUENCY_DROPDOWN_XPATH).first().isVisible();
+		page.locator(FREQUENCY_DROPDOWN_XPATH).first().click();
+
+	}
+
+	public void selectWeeklyValueDropdown(String dropdownValue) {
+		page.locator(WEEKELY_VALUE_XPATH.replace("{dropdown_option}", dropdownValue)).isVisible();
+		page.locator(WEEKELY_VALUE_XPATH.replace("{dropdown_option}", dropdownValue)).click();
+	}
+
+	public String getModelLimitValue(String limit) {
+		page.locator(MODEL_LIMIT_XPATH.replace("{limitValue}", limit)).isVisible();
+		return page.locator(MODEL_LIMIT_XPATH.replace("{limitValue}", limit)).textContent();
+	}
+
+	public void checkSearchButton() {
+		page.isVisible(SEARCH_ICON_XPATH);
+		page.click(SEARCH_ICON_XPATH);
+	}
+
+	public void searchUser() {
+		page.fill(SEARCH_BUTTON_XPATH, "UserId");
+	}
+
+	public void clickSelectAllButton() {
+		page.isVisible(SELECT_ALL_BUTTON_XPATH);
+		page.click(SELECT_ALL_BUTTON_XPATH);
+	}
+
+	public void clickDeleteSelectedButton() {
+		page.isVisible(DELETE_SELECTED_BUTTON_XPATH);
+		page.click(DELETE_SELECTED_BUTTON_XPATH);
+	}
+
+	public String userDeletionToastMessage() {
+		String toastMessage = page.textContent(DELETE_MEMBER_TOAST_MESSAGE_XPATH).trim();
 		return toastMessage;
 	}
 
--- src/test/java/aicore/steps/UserManagementSteps.java
+++ src/test/java/aicore/steps/UserManagementSteps.java
@@ -4,19 +4,23 @@ import org.junit.jupiter.api.Assertions;
 
 import aicore.hooks.SetupHooks;
 import aicore.pages.HomePage;
+import aicore.pages.SettingPage;
 import aicore.pages.UserManagementPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class UserManagementSteps {
 
 	private HomePage homePage;
 	private UserManagementPage userpage;
+	private SettingPage settingpage;
 
 	public UserManagementSteps() {
 		this.homePage = new HomePage(SetupHooks.getPage());
 		this.userpage = new UserManagementPage(SetupHooks.getPage());
+		this.settingpage = new SettingPage(SetupHooks.getPage());
 	}
 
 	@Given("User logs as Admin user in AI CORE application")
@@ -31,20 +35,15 @@ public class UserManagementSteps {
 
 	@And("User enables admin mode")
 	public void user_enables_admin_mode() {
-		if (userpage.checkAdminButton()) {
-			userpage.clickAdminButton();
-		} else {
-			System.out.println("Admin button is not visible");
-		}
+		userpage.checkAdminButton();
+		userpage.clickAdminButton();
 	}
 
-	@And("User clicks Member Settings")
-	public void user_clicks_member_settings() {
-		if (userpage.checkMemberSettingPageTile()) {
-			userpage.clickMemberSettingPageTile();
-		} else {
-			System.out.println("Admin mode is not ON Or Member Setting card is not visible");
-		}
+	@When("User clicks on {string} Card")
+	public void user_clicks_on_card(String cardName) {
+		settingpage.checkCardVisible(cardName);
+		settingpage.clickOnCard(cardName);
+
 	}
 
 	@Then("User sees the Add User button")
@@ -60,7 +59,6 @@ public class UserManagementSteps {
 			userpage.clickTypeDropdown();
 			userpage.clickNativeDropdownValue();
 			userpage.fillUserId("UserId" + i);
-			userpage.fillUsername("Username" + i);
 			userpage.fillName("Name" + i);
 			userpage.fillEmail("UserEmail" + i + "@testautomation.com");
 			if (i <= 9) {
@@ -74,6 +72,77 @@ public class UserManagementSteps {
 			String actualMessage = userpage.userCreationToastMessage();
 			Assertions.assertEquals(actualMessage, toastMessage, "User creation failed");
 		}
+	}
+
+	@And("User clicks on Edit icon")
+	public void user_clicks_on_edit_icon() {
+		userpage.clickOnEditUser();
+	}
+
+	@And("User clicks on Model dropdown")
+	public void user_clicks_on_model_dropdown() {
+		userpage.clickModelUserDropdown();
+	}
+
+	@Then("User selects {string} value in Model dropdown")
+	public void user_selects_value_in_model_dropdown(String dropdownOption) {
+		userpage.selectTokenValueDropdown(dropdownOption);
+	}
+
+	@And("User fills {string} value in Max Tokens field")
+	public void user_fills_value_in_max_tokens_field(String value) {
+		userpage.fillMaxTokensValue(value);
+	}
+
+	@And("User clicks on Frequency dropdown")
+	public void user_clicks_on_frequency_dropdown() {
+		userpage.clickFrequencyDropdown();
+	}
+
+	@And("User selects {string} value in Frequency dropdown")
+	public void user_selects_value_in_frequency_dropdown(String dropdownValue) {
+		userpage.selectWeeklyValueDropdown(dropdownValue);
+	}
+
+	@And("User clicks on save button")
+	public void user_clicks_on_save_button() {
+		userpage.clickSaveButton();
+	}
+
+	@Then("User sees the new model limit value as {string} on Member Settings page")
+	public void user_sees_the_new_model_limit_value_as_on_member_settings_page(String expectedLimitValue)
+			throws InterruptedException {
+		String actualLimitValue = userpage.getModelLimitValue(expectedLimitValue);
+		Assertions.assertEquals(expectedLimitValue, actualLimitValue);
+	}
+
+	@And("User sees the search button")
+	public void user_sees_the_search_button() {
+		userpage.checkSearchButton();
+	}
+
+	@And("User searches for the created user")
+	public void user_searches_for_the_created_user() {
+		userpage.searchUser();
+	}
+
+	@And("User clicks on Delete Selected button {int} times")
+	public void user_clicks_on_delete_selected_button(int usercount) {
+		if (usercount <= 25) {
+			userpage.clickSelectAllButton();
+			userpage.clickDeleteSelectedButton();
+		} else {
+			for (int i = 1; i <= (int) Math.ceil((double) usercount / 25); i++) {
+				userpage.clickSelectAllButton();
+				userpage.clickDeleteSelectedButton();
+			}
+		}
+	}
+
+	@Then("User sees delete toast message as {string}")
+	public void user_sees_delete_toast_message_as(String toastMessage) {
+		String actualMessage = userpage.userDeletionToastMessage();
+		Assertions.assertEquals(actualMessage, toastMessage, "User deletion failed");
 	}
 
 }
