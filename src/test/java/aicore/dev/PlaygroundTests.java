fe026b9d692302a04302a1ba0f9186415a53a34f
Yang <kyang4@deloitte.com>
2025-04-23 16:57:20 -0400

    playground progress

--- src/test/java/aicore/dev/PlaygroundTests.java
+++ src/test/java/aicore/dev/PlaygroundTests.java
@@ -1,12 +1,16 @@
 package aicore.dev;
 
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.ElementHandle;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.Tracing;
+import com.microsoft.playwright.options.WaitForSelectorState;
 
 import aicore.base.GenericSetupUtils;
 import aicore.utils.ConfigUtils;
@@ -33,8 +37,7 @@ public class PlaygroundTests {
 		page.setDefaultTimeout(Double.parseDouble(ConfigUtils.getValue("timeout")));
 
 		GenericSetupUtils.setupLoggers(page);
-		// run once
-//		GenericSetupUtils.createUsers(page);
+
 		String adminUser = ConfigUtils.getValue("admin_username");
 		String adminPassword = ConfigUtils.getValue("admin_password");
 		
@@ -42,14 +45,79 @@ public class PlaygroundTests {
 		GenericSetupUtils.login(page, adminUser, adminPassword);
 		
 		// TODO go to settings page
+		page.getByTestId("Settings-icon").click();
 		
 		// click on model settings
+		page.getByText("Model Settings").click();
+		
+		// Toggle to Admin On
+		page.getByText("Admin Off").click();
 		
 		// assign user to every model
 		
+		// Selector for the container
+        String containerSelector = ".MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-3.css-1h77wgb";
+        // Selector for the items within the container
+        String itemSelector = ".MuiGrid-root.MuiGrid-item.MuiGrid-grid-sm-12.MuiGrid-grid-md-6.MuiGrid-grid-lg-4.MuiGrid-grid-xl-3.css-1noz1rq";
+
+        // Get the container element
+        ElementHandle container = page.querySelector(containerSelector);
+
+       
+        // Get all items within the container
+        java.util.List<ElementHandle> items = container.querySelectorAll(itemSelector);
+
+        /*
+        // Iterate through each item and click on it
+        for (ElementHandle item : items) {
+            item.click();
+            page.getByText("Add Members").click();
+            
+            
+
+            // Locate the modal container
+            Locator modal = page.locator(".MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation24.MuiDialog-paper.MuiDialog-paperScrollPaper.MuiDialog-paperWidthLg.css-4p7mgu[role='dialog'][aria-labelledby=':rc3:']");
+            
+            
+            page.getByPlaceholder("Search Users").click();
+            page.getByPlaceholder("Search Users").fill(adminUser);
+            
+            // Selector for the text within the modal
+            String textSelector = "text=admin lastname";
+            
+            if (modal.locator(textSelector).isVisible()) {
+            	page.getByText("admin lastname").click();
+            	page.querySelector("input.PrivateSwitchBase-input.css-1m9pwf3[name=':rs:'][type='radio'][value='Author']").click();
+                page.getByText("Save").click();
+            } else {
+            	page.getByText("Cancel").click();
+            }
+            
+            page.getByText("Model Settings").click();
+            
+         
+
+        }
+       */
 		
+        // import playground v4
+        page.click("//a[@data-tour='nav-app-library']");
+        page.getByText("Create New App").click();
+        page.getByText("Upload").click();
+        
+        // Locate the file input element associated with the "Browse" button
+        Locator fileInput = page.locator("input[type='file']");
+
+        // Set the file to be uploaded
+        fileInput.setInputFiles(Paths.get(ConfigUtils.getValue("playgroundAppFile")));
+  
+        page.getByText("Next").click();
+        page.getByText("Name").click();
+        page.getByText("Name").fill("Playground v04");
+        page.getByText("Next").click();
 		
 		// navigate to playground v4
+        
 		
 		// write out test cases for each playground test
 		
