ab49a027456f1a5472830a21bf47a92d70a95f24
Saini <abhissaini@deloitte.com>
2025-04-30 17:01:36 +0530

    issue 138 - add function using zip file

---
+++ src/test/java/aicore/steps/AddFunctionSteps.java
@@ -0,0 +1,135 @@
+package aicore.steps;
+
+import org.junit.jupiter.api.Assertions;
+
+import aicore.hooks.SetupHooks;
+import aicore.pages.AddFunctionToCatalogPage;
+import aicore.pages.HomePage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AddFunctionSteps {
+    private HomePage homePage;
+    private AddFunctionToCatalogPage addFunctionToCatalogPage;
+
+    public AddFunctionSteps() {
+        homePage = new HomePage(SetupHooks.getPage());
+        addFunctionToCatalogPage = new AddFunctionToCatalogPage(SetupHooks.getPage());
+    }
+
+    @Given("User navigates to Open Function")
+    public void user_navigates_to_open_function() {
+        homePage.clickOnOpenFunction();
+    }
+
+    @When("User clicks on Add Function")
+    public void user_clicks_on_add_function() {
+        addFunctionToCatalogPage.clickOnAddFunctionButton();
+
+    }
+
+    @Then("User selects function {string}")
+    public void user_selects_function(String functionType) {
+        addFunctionToCatalogPage.selectFunction(functionType);
+    }
+
+    @Then("User uploads function file {string}")
+    public void user_uploads_funtion_file(String fileName) {
+        String uploadedFileName = addFunctionToCatalogPage.enterFilePath(fileName);
+        if (fileName.contains("/")) {
+            String[] ActualFileName = fileName.split("/");
+            int fileNameIndex = ActualFileName.length - 1;
+            Assertions.assertEquals(ActualFileName[fileNameIndex], uploadedFileName,
+                    "function Document file is not uploaded successfully");
+        } else {
+            Assertions.assertEquals(fileName, uploadedFileName, "function Document file is not uploaded successfully");
+        }
+    }
+
+    @And("User sees astrisk mark on the required fields {string}")
+    public void user_sees_astrisk_mark_on_the_required_fields(String requiredFields) {
+        addFunctionToCatalogPage.verifyAsteriskMarkOnFields(requiredFields);
+    }
+
+    @And("User enters Catalog name {string}")
+    public void user_enters_catalog_name(String catalogName) {
+        addFunctionToCatalogPage.enterCatalogName(catalogName);
+    }
+
+    @And("User enters Url as {string}")
+    public void user_enters_url_as(String url) {
+        addFunctionToCatalogPage.enterUrl(url);
+    }
+
+    @And("User selects HTTP method as {string}")
+    public void user_selects_http_method_as(String httpMethod) {
+        addFunctionToCatalogPage.selectHttpMethod(httpMethod);
+    }
+
+    @And("User selects Post body message as {string}")
+    public void user_selects_post_body_message_as(String postBodyMessage) {
+        addFunctionToCatalogPage.selectPostBodyMessage(postBodyMessage);
+    }
+
+    @And("User enters Headers as {string}")
+    public void user_enters_headers_as(String headers) {
+        addFunctionToCatalogPage.enterHeaders(headers);
+    }
+
+    @And("User enters Function parameters as {string}")
+    public void user_enters_function_parameters_as(String functionParameters) {
+        addFunctionToCatalogPage.enterFunctionParameters(functionParameters);
+    }
+
+    @And("User enters Function required parameters as {string}")
+    public void user_enters_function_required_parameters_as(String functionRequiredParameters) {
+        addFunctionToCatalogPage.enterFunctionRequiredParameters(functionRequiredParameters);
+
+    }
+
+    @And("User enters Function name as {string}")
+    public void user_enters_function_name_as(String functionName) {
+        addFunctionToCatalogPage.enterFunctionName(functionName);
+    }
+
+    @And("User enters Function description as {string}")
+    public void user_enters_function_description_as(String functionDescription) {
+        addFunctionToCatalogPage.enterFunctionDescription(functionDescription);
+    }
+
+    @Then("User clicks on Create Funtion button")
+    public void user_sees_create_Funtion_button() {
+        addFunctionToCatalogPage.clickOnCreateFunctionButton();
+    }
+
+    @Then("User sees the function name {string} in the function catalog")
+    public void user_sees_the_function_name_in_the_catalog(String functionName) {
+        String addedFunction = addFunctionToCatalogPage.verifyFunctionNameInCatalog(functionName);
+        Assertions.assertEquals(functionName, addedFunction, "Function is not present in the function catalog");
+    }
+
+    @Then("User clicks on the function name {string} in the function catalog")
+    public void user_clicks_on_the_function_name_in_the_function_catalog(String functionName) {
+        addFunctionToCatalogPage.clickOnFunctionNameInCatalog(functionName);
+    }
+
+    @Then("User clicks on Access Control Tab")
+    public void user_clicks_on_access_control_tab() {
+        addFunctionToCatalogPage.clickOnAccessControl();
+    }
+
+    @Then("User clicks on Delete button")
+    public void user_clicks_on_delete_button() {
+        addFunctionToCatalogPage.clickOnDeleteButton();
+        addFunctionToCatalogPage.clickOnDeleteConfirmationButton();
+    }
+
+    @Then("User sees deleted function success toast message {string}")
+    public void user_sees_seleted_function_success_toast_message(String toastMessage) {
+        String expectedMessage = addFunctionToCatalogPage.verifyDeleteToastMessage();
+        String actualMessage = toastMessage;
+        Assertions.assertEquals(actualMessage, expectedMessage, "Delete Message is not matching with expected");
+    }
+}
