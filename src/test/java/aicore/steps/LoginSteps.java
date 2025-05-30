1bbbf39745d90d997b48476692309c32d689f3d9
roxy <rocioram94@gmail.com>
2025-05-29 22:10:22 -0400

    login step code cleanup

--- src/test/java/aicore/steps/LoginSteps.java
+++ src/test/java/aicore/steps/LoginSteps.java
@@ -23,33 +23,25 @@ public class LoginSteps {
 	private HomePage homePage;
 
 	public LoginSteps() {
-//		Page page = new AICoreTestBase().page; 
 		this.loginpage = new LoginPage(SetupHooks.getPage());
 		this.homePage = new HomePage(SetupHooks.getPage());
 	}
 
 	@Given("User is on application")
 	public void user_is_on_application() throws IOException {
-
 		loginpage.navigateToLoginPage();
-
 	}
 
 	@When("User enters username and password and click on SignIn button")
 	public void user_enters_username_and_password_and_click_on_sign_in_button()
 			throws InterruptedException, IOException {
-
 		loginpage.closeCookiesPopup();
 		loginpage.loginToApplication();
 	}
 
 	@Then("User can navigate to home page")
 	public void user_can_navigate_to_home_page() throws InterruptedException {
-//		homePage.closeInfoPopup();
 		homePage.navigateToHomePage();
-//		String actTitle = .getPageTitle();
-//		Assert.assertEquals(ConfigUtils.getValue("applicationName"), actTitle);
-
 	}
 
 	@When("User enters nativeUsername and nativePassword")
@@ -57,7 +49,6 @@ public class LoginSteps {
 			throws InterruptedException {
 		loginpage.closeCookiesPopup();
 		loginpage.enterNativeUsernamePassword();
-
 	}
 
 	@And("User clicks on Login with native button")
