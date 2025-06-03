89e5cd21b4c675eba3c02701910536e101f0630e
hchoramale <hchoramale@deloitte.com>
2025-06-03 16:39:34 +0530

    Issue-123 view, filter and bookmark database on catalog page

--- src/main/java/aicore/pages/AddDatabasePage.java
+++ src/main/java/aicore/pages/AddDatabasePage.java
@@ -5,34 +5,66 @@ import com.microsoft.playwright.Page;
 import aicore.utils.AddDatabasePageUtils;
 
 public class AddDatabasePage {
-    private Page page;
+	private Page page;
 
-    public AddDatabasePage(Page page) {
-        this.page = page;
-    }
+	public AddDatabasePage(Page page) {
+		this.page = page;
+	}
 
-    public void clickOnAddDatabaseButton() {
-        AddDatabasePageUtils.clickAddDatabaseButton(page);
-    }
+	public void clickOnAddDatabaseButton() {
+		AddDatabasePageUtils.clickAddDatabaseButton(page);
+	}
 
-    public void selectDatabaseType(String dbType) {
-        AddDatabasePageUtils.selectDatabaseType(page, dbType);
-    }
+	public void selectDatabaseType(String dbType) {
+		AddDatabasePageUtils.selectDatabaseType(page, dbType);
+	}
 
-    public String uploadDatabaseFile(String fileName) {
-        return AddDatabasePageUtils.uploadDatabaseFile(page, fileName);
-    }
+	public String uploadDatabaseFile(String fileName) {
+		return AddDatabasePageUtils.uploadDatabaseFile(page, fileName);
+	}
 
-    public void clickOnCreateDatabaseButton() {
-        AddDatabasePageUtils.clickCreateDatabaseButton(page);
-    }
+	public void clickOnCreateDatabaseButton() {
+		AddDatabasePageUtils.clickCreateDatabaseButton(page);
+	}
 
-    public String verifyDatabaseNameInCatalog(String dbName) {
-        return AddDatabasePageUtils.verifyDatabaseNameInCatalog(page, dbName);
-    }
+	public String verifyDatabaseNameInCatalog(String dbName) {
+		return AddDatabasePageUtils.verifyDatabaseNameInCatalog(page, dbName);
+	}
 
-    public void clickOnDatabaseNameInCatalog(String dbName) {
-        AddDatabasePageUtils.clickOnDatabaseNameInCatalog(page, dbName);
-    }
+	public void clickOnDatabaseNameInCatalog(String dbName) {
+		AddDatabasePageUtils.clickOnDatabaseNameInCatalog(page, dbName);
+	}
+
+	public boolean verifyDatabaseIsVisbileInCatalog(String dbName) {
+		return AddDatabasePageUtils.verifyDatabaseIsVisbileInCatalog(page, dbName);
+	}
+
+	public void clickOnCopyID(String dbName) {
+		AddDatabasePageUtils.clickOnCopyID(page, dbName);
+	}
+
+	public String verifyCopyIdSuccessToastMessage(String toastMessage) {
+		return AddDatabasePageUtils.verifyCopyIdSuccessToastMessage(page, toastMessage);
+	}
+
+	public void searchFilterValue(String filterValue) {
+		AddDatabasePageUtils.searchFilterValue(page, filterValue);
+	}
+
+	public void selectFilterValue(String filterCategory, String filterValue) {
+		AddDatabasePageUtils.selectFilterValue(page, filterCategory, filterValue);
+	}
+
+	public void clickOnBookmark(String catalogName) {
+		AddDatabasePageUtils.clickOnBookmark(page, catalogName);
+	}
+
+	public void clickOnUnbookmark(String catalogName) {
+		AddDatabasePageUtils.clickOnUnbookmark(page, catalogName);
+	}
+
+	public boolean verifyCatalogDisplayedUnderBookmarkedSection(String catalaogName) {
+		return AddDatabasePageUtils.verifyCatalogDisplayedUnderBookmarkedSection(page, catalaogName);
+	}
 
 }
--- src/main/java/aicore/utils/AddDatabasePageUtils.java
+++ src/main/java/aicore/utils/AddDatabasePageUtils.java
@@ -5,6 +5,8 @@ import java.nio.file.Paths;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.WaitForSelectorState;
 
 public class AddDatabasePageUtils {
 
@@ -12,6 +14,12 @@ public class AddDatabasePageUtils {
 	private static final String ADD_FILE_XPATH = "//input[@type='file']";
 	private static final String ADD_FILE_NAME_XPATH = "//span[@title='{fileName}']";
 	private static final String CREATE_DATABASE_BUTTON_LABEL = "Create database";
+	private static final String VERTICAL_OPTIONS_XPATH = "//button[contains(@title, '{catalogName}')]/following-sibling::button/*[name()='svg']";
+	private static final String COPY_ID_OPTION_TEXT = "Copy";
+	private static final String SELECT_FILTER_VALUE_XPATH = "//h6[text()='{filterCategory}']/ancestor::li/following-sibling::div//p[text()='{filterValue}']";
+	private static final String BOOKMARK_ICON_XPATH = "//button[contains(@title, '{catalogName}')]/*[name()='svg']";
+	private static final String UNBOOKMARK_ICON_DATA_TEST_ID = "BookmarkIcon";
+	private static final String CATALOG_UNDER_BOOKMARKED_SECTION_XPATH = "//h6[text()='Bookmarked']/following-sibling::div[1]//p[text()='{catalogName}']";
 
 	public static void clickAddDatabaseButton(Page page) {
 		page.getByLabel(ADD_DATABASE_BUTTON).isVisible();
@@ -59,4 +67,45 @@ public class AddDatabasePageUtils {
 	public static void clickOnDatabaseNameInCatalog(Page page, String dbName) {
 		page.getByText(dbName).click();
 	}
+
+	public static boolean verifyDatabaseIsVisbileInCatalog(Page page, String dbName) {
+		boolean isFunctionVisible = page.getByText(dbName).isVisible();
+		return isFunctionVisible;
+	}
+
+	public static void clickOnCopyID(Page page, String catalogName) {
+		page.locator(VERTICAL_OPTIONS_XPATH.replace("{catalogName}", catalogName)).click();
+		page.getByRole(AriaRole.MENUITEM, new Page.GetByRoleOptions().setName(COPY_ID_OPTION_TEXT)).click();
+	}
+
+	public static String verifyCopyIdSuccessToastMessage(Page page, String toastMessage) {
+		Locator toastAlert = page.getByRole(AriaRole.ALERT)
+				.filter(new Locator.FilterOptions().setHasText(toastMessage));
+		toastAlert.waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
+		return toastAlert.textContent().trim();
+	}
+
+	public static void searchFilterValue(Page page, String filterValue) {
+		page.getByPlaceholder("Search by...").fill(filterValue);
+	}
+
+	public static void selectFilterValue(Page page, String filterCategory, String filterValue) {
+		Locator filterValueLocator = page.locator(SELECT_FILTER_VALUE_XPATH.replace("{filterCategory}", filterCategory)
+				.replace("{filterValue}", filterValue));
+		filterValueLocator.waitFor();
+		filterValueLocator.click();
+	}
+
+	public static void clickOnBookmark(Page page, String catalogName) {
+		page.locator(BOOKMARK_ICON_XPATH.replace("{catalogName}", catalogName)).click();
+	}
+
+	public static void clickOnUnbookmark(Page page, String catalogName) {
+		page.locator(BOOKMARK_ICON_XPATH.replace("{catalogName}", catalogName)).first().click();
+	}
+
+	public static boolean verifyCatalogDisplayedUnderBookmarkedSection(Page page, String catalogName) {
+		return page.locator(CATALOG_UNDER_BOOKMARKED_SECTION_XPATH.replace("{catalogName}", catalogName)).isVisible();
+	}
+
 }
