b3a34baf0d3b6c011b64c1f5ade15cf5aecf107e
pranchaudhari <pranaleechaudhari1@gmail.com>
2025-08-26 14:59:21 +0530

    test(341): fix

--- src/test/java/aicore/steps/app/createAppUsingTemplateSteps.java
+++ src/test/java/aicore/steps/app/createAppUsingTemplateSteps.java
@@ -1,7 +1,10 @@
 package aicore.steps.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import aicore.hooks.SetupHooks;
 import aicore.pages.app.AppTemplatePage;
+import aicore.utils.UrlUtils;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -76,6 +79,43 @@ public void user_views_description_as(String description) {
 }
 @Then("User sees the hyperlink with text {string} should point to {string}")
 public void user_sees_the_hyperlink_with_text_should_point_to(String text, String url) {
-    appTemplatePage.verifyHyperlink(text, url);
+        appTemplatePage.verifyHyperlink(text, url);
+        String currentUrl = appTemplatePage.getCurrentUrl();
+        String actualRelativePath = UrlUtils.extractRelativePath(currentUrl);
+        assertEquals(url, actualRelativePath, "URL mismatch!");
+    }
+    @Then("User navigates to back page")
+public void user_navigates_to_back_page() {
+    appTemplatePage.getBackPage();
+}
+
+@Then("User views description for the block with title {string} as {string}")
+public void user_views_description_for_the_block_with_title_as(String blockTitle, String description) {
+    appTemplatePage.verifyDescriptionBelowTitleOfBlock(blockTitle, description);
+}
+@Then("User clicks on the hyperlink with text {string}  with title {string} should point to {string}")
+public void user_clicks_on_the_hyperlink_with_text_with_title_should_point_to(String text, String blockTitle, String url) {
+  appTemplatePage.verifyHyperlinkText(text,blockTitle,url);
+   String currentUrl = appTemplatePage.getCurrentUrl();
+        String actualRelativePath = UrlUtils.extractRelativePath(currentUrl);
+        assertEquals(url, actualRelativePath, "URL mismatch!");
+    }
+    @Then("User clicks on hyperlink text {string}")
+public void user_clicks_on_hyperlink_text(String text) {
+     appTemplatePage.clickOnHyperlinkText(text);
+}
+@Then("User filles the destination URL as {string}")
+public void user_filles_the_destination_url_as(String url) {
+    appTemplatePage.fillDestinationUrl(url);
+}
+@Then("User clicks on Save button of the app")
+public void user_clicks_on_save_button_of_the_app() {
+    appTemplatePage.clickSaveButtonOfTheApp();
+}
+@Then("User sees the URL as {string}")
+public void user_sees_the_url_as(String expectedUrl) {
+       String actualUrl = appTemplatePage.getCurrentUrl();
+       assertEquals(expectedUrl, actualUrl,
+        "Expected URL does not match the current page URL.");
 }
 }
