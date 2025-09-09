d51927129d34a2096085e507b8276949ea3b1ff2
Saini <abhissaini@deloitte.com>
2025-09-09 18:49:06 +0530

    feat(issue-368): Add team member in Team permission

--- src/test/java/aicore/steps/TeamPermissionsSettingSteps.java
+++ src/test/java/aicore/steps/TeamPermissionsSettingSteps.java
@@ -38,6 +38,33 @@ public class TeamPermissionsSettingSteps {
     public void user_clicks_on_button_in_add_team_form(String button) {
         teamPermissionsSettings.clickOnAddButton(button);
     }
+    
+    @When("User clicks on {string} button in Add Team Page")
+    public void user_clicks_on_button_in_add_team_page(String button) {
+        teamPermissionsSettings.clickOnAddTeamButton(button);
+    }
+
+    @When("User selects {string} member from the list")
+    public void user_selects_member_from_the_list(String member) {
+        teamPermissionsSettings.selectMemberFromList(member);
+    }
+    @When("User sees {string} card in the Add Member form")
+    public void user_sees_card_in_the_add_member_form(String member) {
+        teamPermissionsSettings.checkMemberCard(member);
+    }
+
+    @When("User clicks on {string} button in Add Member form")
+    public void user_clicks_on_button_in_add_member_form(String button) {
+        teamPermissionsSettings.clickOnSaveMemberButton(button);
+    }
+    @Then("User sees the message {string} is displayed")
+    public void user_sees_the_message_is_displayed(String expectedMessage) {
+        teamPermissionsSettings.validateToastMessage(expectedMessage);
+    }
+    @Then("User can see the new member {string} added in the team member list")
+    public void user_can_see_the_new_member_added_in_the_team_member_list(String member) {
+        teamPermissionsSettings.checkMemberInList(member);
+    }
 
     @Then("User can see team name as {string} in the list")
     public void user_can_see_team_name_as_in_the_list(String name) {
