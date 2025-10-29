0e366f583f28a06c75504f11b1a59cb8e7a10118
Jeff Vitunac <jvitunac@gmail.com>
2025-10-29 15:56:57 -0400

    chore: coverage moved to workspace, removed old build files, fixed current ones

--- tester_para.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker compose -f dcp.yaml --env-file .env --env-file .env.local up --remove-orphans --build semoss;
-
-docker compose -f dcp.yaml --env-file .env --env-file .env.local up --remove-orphans --build --exit-code-from test test;
