c9d82af4050d31edc5916f1719a14087a8d50a13
hchoramale <hchoramale@deloitte.com>
2025-10-30 19:19:15 +0530

    fix: enabled regression tag

--- src/test/java/aicore/suite/FullSuite.java
+++ src/test/java/aicore/suite/FullSuite.java
@@ -10,7 +10,7 @@ import org.junit.platform.suite.api.Suite;
 @SelectClasspathResource("Features")
 @ConfigurationParameter(key = "cucumber.glue", value = "aicore.steps,aicore.hooks")
 //@ConfigurationParameter(key = "cucumber.glue", value =  "aicore.hooks")
-//@ConfigurationParameter(key = "cucumber.filter.tags", value = "@Regression")
+@ConfigurationParameter(key = "cucumber.filter.tags", value = "@Regression")
 // @ConfigurationParameter(key = "cucumber.filter.tags", value = "@Documentation")
 // @ConfigurationParameter(key = "cucumber.filter.tags", value = "@Smoke")
 @ConfigurationParameter(key = "cucumber.plugin", value = "pretty,html:target/cucumber-report.html")
