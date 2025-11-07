3dcfacfabc49cd5a05c07633492101c25cae114c
hchoramale <hchoramale@deloitte.com>
2025-11-07 13:59:52 +0530

    fixed all scenarios

--- src/test/java/aicore/suite/FullSuite.java
+++ src/test/java/aicore/suite/FullSuite.java
@@ -11,7 +11,7 @@ import org.junit.platform.suite.api.Suite;
 @ExcludeTags("ApplicationBugFailure")
 @SelectClasspathResource("Features")
 @ConfigurationParameter(key = "cucumber.glue", value = "aicore.steps,aicore.hooks")
-@ConfigurationParameter(key = "cucumber.filter.tags", value = "@Regression,@Documentation")
+@ConfigurationParameter(key = "cucumber.filter.tags", value = "@Regression")
 // @ConfigurationParameter(key = "cucumber.filter.tags", value = "@Documentation")
 // @ConfigurationParameter(key = "cucumber.filter.tags", value = "@Smoke")
 @ConfigurationParameter(key = "cucumber.plugin", value = "pretty,html:target/cucumber-report.html")
