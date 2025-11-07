2a4fa70e3df9eb2d20997a9529552c1971c4d93e
Jeff Vitunac <jvitunac@gmail.com>
2025-11-07 09:00:52 -0500

    test: fixing up suite class

--- src/test/java/aicore/suite/FullSuite.java
+++ src/test/java/aicore/suite/FullSuite.java
@@ -7,7 +7,7 @@ import org.junit.platform.suite.api.*;
 @ExcludeTags("ApplicationBugFailure")
 @SelectClasspathResource("Features")
 @ConfigurationParameter(key = "cucumber.glue", value = "aicore.steps,aicore.hooks")
-@ConfigurationParameter(key = "cucumber.filter.tags", value = "@Regression,@Documentation")
+@ConfigurationParameter(key = "cucumber.filter.tags", value = "@Regression or @Documentation")
 // @ConfigurationParameter(key = "cucumber.filter.tags", value = "@Documentation")
 // @ConfigurationParameter(key = "cucumber.filter.tags", value = "@Smoke")
 @ConfigurationParameter(key = "cucumber.plugin", value = "pretty,html:target/cucumber-report.html")
