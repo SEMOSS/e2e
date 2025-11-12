1d61b2f3b8d52755b8c2b1783bd0b34722ba1545
Jeff Vitunac <jvitunac@gmail.com>
2025-11-12 15:37:49 -0500

    chore: returning to old test suite

--- src/test/java/aicore/suite/FullSuite.java
+++ src/test/java/aicore/suite/FullSuite.java
@@ -11,8 +11,8 @@ import org.junit.platform.suite.api.Suite;
 @ExcludeTags("ApplicationBugFailure")
 @SelectClasspathResource("Features")
 @ConfigurationParameter(key = "cucumber.glue", value = "aicore.steps,aicore.hooks")
-//@ConfigurationParameter(key = "cucumber.filter.tags", value = "@Regression or @Documentation")
-@ConfigurationParameter(key = "cucumber.filter.tags", value = "@Documentation")
+@ConfigurationParameter(key = "cucumber.filter.tags", value = "@Regression or @Documentation")
+//@ConfigurationParameter(key = "cucumber.filter.tags", value = "@Documentation")
 // @ConfigurationParameter(key = "cucumber.filter.tags", value = "@Smoke")
 @ConfigurationParameter(key = "cucumber.plugin", value = "pretty,html:target/cucumber-report.html")
 public class FullSuite {
