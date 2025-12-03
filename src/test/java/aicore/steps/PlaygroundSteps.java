432e6fb1c25840f57de39502ba847b7b50520cf1
Saini <abhissaini@deloitte.com>
2025-12-03 17:28:28 +0530

    feat(591): Playground configuration panel  model validation

--- src/test/java/aicore/steps/PlaygroundSteps.java
+++ src/test/java/aicore/steps/PlaygroundSteps.java
@@ -2,8 +2,6 @@ package aicore.steps;
 
 import java.util.List;
 
-import org.junit.platform.commons.function.Try;
-
 import aicore.hooks.SetupHooks;
 import aicore.pages.PlaygroundPage;
 import io.cucumber.datatable.DataTable;
@@ -45,6 +43,36 @@ public class PlaygroundSteps {
     @And("User clicks on the {string} button")
     public void user_Clicks_On_The_Open_Configuration_Menu_Button(String buttonName) {
         playgroundPage.clickOnOpenConfigurationMenuButton(buttonName);
+    }
+
+     @Then("User verify the model catalog dropdown is present with default model with {string} name")
+    public void user_Verify_The_Model_Catalog_Dropdown_Is_Present_With_Default_Model_With(String modelName) {
+        playgroundPage.verifyModelCatalogDropdownPresent(modelName);
+    }
+
+    @When("User clicks on the Model dropdown")
+    public void user_Clicks_On_The_Model_Dropdown() {
+        playgroundPage.clickOnModelCatalogDropdown();
+    }
+
+    @Then("User verify {string} model should be checked in the model catalog dropdown")
+    public void user_Verify_The_Model_Should_Be_Checked_In_The_Model_Catalog_Dropdown(String modelName) {
+        playgroundPage.verifyModelIsChecked(modelName);
+    }
+
+    @When("User searches the {string} configuration tab in the model catalog searchbox")
+    public void user_Searches_The_Configuration_Tab_In_The_Model_Catalog_Searchbox(String modelName) {
+        playgroundPage.searchModelInSearchbox(modelName);
+    }
+
+    @Then("User should see the {string} in the model catalog dropdown")
+    public void user_Should_See_The_In_The_Model_Catalog_Dropdown(String modelName) {
+        playgroundPage.verifyModelVisibleInDropdown(modelName);
+    }
+
+    @When("User selects the {string} from the model catalog dropdown")
+    public void user_Selects_The_From_The_Model_Catalog_Dropdown(String modelName) {
+        playgroundPage.selectModelFromDropdown(modelName);
     }
 
     @Then("User verifies that the {string} button is {string}")
