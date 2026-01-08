3cac5765bd2da49ef3be72eff19107103e2cdd63
Jeff Vitunac <jvitunac@gmail.com>
2026-01-08 10:55:14 -0500

    feat: adding application failure suite, uncommenting out tests that were fixed

---
+++ src/test/java/aicore/suite/ApplicationFailureSuite.java
@@ -0,0 +1,17 @@
+package aicore.suite;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("Features")
+@ConfigurationParameter(key = "cucumber.glue", value = "aicore.steps,aicore.hooks")
+@ConfigurationParameter(key = "cucumber.filter.tags", value = "@ApplicationBugFailure")
+//@ConfigurationParameter(key = "cucumber.filter.tags", value = "@Documentation")
+// @ConfigurationParameter(key = "cucumber.filter.tags", value = "@Smoke")
+@ConfigurationParameter(key = "cucumber.plugin", value = "pretty,html:target/cucumber-report.html")
+public class ApplicationFailureSuite {
+}
