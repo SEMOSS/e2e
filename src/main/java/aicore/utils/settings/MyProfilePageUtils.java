3c1a9b1ca12c82c337851403d5afd26409050f52
BhaktiKanini <bhakti.zurunge@kanini.com>
2026-03-31 22:27:25 +0530

    Feature/at 865 settings my profile validate and edit profile information (#871)
    * Feature file written
    
    * Test(#865):Implement the script for validate the Edit Profile page under
    My profile
    
    * remove the unwanted space

--- src/main/java/aicore/utils/settings/MyProfilePageUtils.java
+++ src/main/java/aicore/utils/settings/MyProfilePageUtils.java
@@ -18,6 +18,13 @@ public class MyProfilePageUtils {
 	private static final String DELETE_KEY_TOAST_MESSAGE_XPATH = "//div[contains(@class, 'MuiAlert-message')]";
 	private static final String GENERATED_KEY_XPATH = "//td[contains(text(),'{keyName}')]";
 	private static final String GENERATED_DESCRIPTION_XPATH = "//td[text()='{description}']";
+	private static final String EDIT_PROFILE_INFORMATION_STATE_XPATH = "//label[text()='{fieldName}']/following::input | //span[text()='{fieldName}']/parent::button";
+	private static final String UPDATED_NAME_XPATH = "//span[text()='{name}']";
+	private static final String CLICK_ON_CHANGE_PASSWORD_LINK_TEXT = "Change Password";
+	private static final String CHANGE_PASSWORD_POP_TITLE_XPATH = "//h6[text()='Change Password']";
+	private static final String CLICK_ON_PROFILE_ICON_XPATH = "//ul[@aria-label='user navigation']";
+	private static final String ENTER_PASSWORD_XPATH = "//div//p[text()='{fieldName}']/following::div//div//input[@type='password']";
+	private static final String PASSWORDS_DO_NOT_MATCH_ERROR_XPATH = "//p[text()='The passwords do not match']";
 
 	public static void clickOnMyProfileCard(Page page) {
 		page.getByText("My profile").click();
@@ -105,5 +112,51 @@ public class MyProfilePageUtils {
 	public static String getExpectedDescriptionName(Page page, String description, String timestamp) {
 		String expDescription = description + timestamp;
 		return expDescription;
+	}
+
+	public static boolean isFieldEnabled(Page page, String fieldName) {
+		Locator fieldState = page.locator(EDIT_PROFILE_INFORMATION_STATE_XPATH.replace("{fieldName}", fieldName))
+				.first();
+		String disabledAttr = fieldState.getAttribute("disabled");
+		if (disabledAttr != null) {
+			return false;
+		}
+		return fieldState.isEnabled();
+	}
+
+	public static void updateField(Page page, String fieldName, String fieldValue) {
+		Locator fieldLocator = page.locator(EDIT_PROFILE_INFORMATION_STATE_XPATH.replace("{fieldName}", fieldName))
+				.first();
+		fieldLocator.clear();
+		fieldLocator.fill(fieldValue);
+	}
+
+	public static boolean getUpdatedInfo(Page page, String name) {
+		Locator updatedName = page.locator(UPDATED_NAME_XPATH.replace("{name}", name)).first();
+		return updatedName.isVisible();
+	}
+
+	public static void clickOnChangePasswordLink(Page page) {
+		page.getByText(CLICK_ON_CHANGE_PASSWORD_LINK_TEXT).click();
+	}
+
+	public static boolean changePasswordTitle(Page page, String title) {
+		Locator titleLocator = page.locator(CHANGE_PASSWORD_POP_TITLE_XPATH);
+		return titleLocator.isVisible();
+	}
+
+	public static void clickOnProfileIcon(Page page) {
+		page.locator(CLICK_ON_PROFILE_ICON_XPATH).click();
+	}
+
+	public static void enterThePassword(Page page, String fieldName, String fieldValue) {
+		Locator passwordField = page.locator(ENTER_PASSWORD_XPATH.replace("{fieldName}", fieldName)).first();
+		passwordField.click();
+		passwordField.fill(fieldValue);
+	}
+
+	public static String getErrorMessage(Page page, String message) {
+		String errorMessage = page.locator(PASSWORDS_DO_NOT_MATCH_ERROR_XPATH).textContent().trim();
+		return errorMessage;
 	}
 }
