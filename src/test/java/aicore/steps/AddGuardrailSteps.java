eec803a6957a4df0f0426af45352cb5ad976a51a
BhaktiKanini <bhakti.zurunge@kanini.com>
2026-04-03 17:29:00 +0530

    VerifySearchfunctionaliguardrail feature file fix

--- src/test/java/aicore/steps/AddGuardrailSteps.java
+++ src/test/java/aicore/steps/AddGuardrailSteps.java
@@ -83,12 +83,12 @@ public class AddGuardrailSteps {
 
 	@Then("User enters {string} in the search box")
 	public void user_enters_in_the_search_box(String searchText) {
-		guardrailPage.searchGuardrailCatalog(searchText);
+		guardrailPage.searchGuardrailCatalog(searchText + timestamp);
 	}
 
 	@Then("User should see the Guardrail Catalog title as {string} in search results")
 	public void user_should_see_the_guardrail_catalog_title_as_in_search_results(String guardrailTitle) {
-		boolean isGuardrailVisible = guardrailPage.verifySearchedGuardrailCatalogTitle(guardrailTitle);
+		boolean isGuardrailVisible = guardrailPage.verifySearchedGuardrailCatalogTitle(guardrailTitle + timestamp);
 		Assertions.assertTrue(isGuardrailVisible, "Guardrail title is not visible in search results");
 	}
 
