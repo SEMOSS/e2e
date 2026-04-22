4e88ec064b30670cc4d61709a66349e35e76457d
roxy <rocioram94@gmail.com>
2026-04-22 11:14:43 -0400

    feat: adding fe/be bug tags

--- src/test/java/aicore/utils/TestTags.java
+++ src/test/java/aicore/utils/TestTags.java
@@ -4,5 +4,9 @@ public class TestTags {
 	public static final String SMOKE = "smoke";
 	public static final String REGRESSION = "regression";
 	public static final String BROKEN = "broken";
+	
+	public static final String BE_BUG = "beBug";
+	public static final String FE_BUG = "feBug";
+
 
 }
