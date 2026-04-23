5ff2ea33abee00fad02598a2b31a0e04819ab958
jachetheboss <77856102+jachetheboss@users.noreply.github.com>
2026-04-23 08:32:40 -0400

    Adding database and insight creation testing for Excel Option in BIAppTests.java

--- src/test/java/aicore/unit/app/system/BIAppTests.java
+++ src/test/java/aicore/unit/app/system/BIAppTests.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import com.microsoft.playwright.Locator;
@@ -25,7 +26,8 @@ import aicore.utils.page.app.CreateAppPopupUtils;
 
 public class BIAppTests extends AbstractE2ETest {
 	private static String appName = "Test app";
-	private static String databaseName = "DB created from CSV";
+	private static String databaseNameCSV = "DB created from CSV";
+	private static String databaseNameExcel = "DB created from Excel";
 	private static String insightName = "Test Automation";
 	private static String timestamp = CommonUtils.getTimeStampName();
 	
@@ -36,7 +38,7 @@ public class BIAppTests extends AbstractE2ETest {
 
 
 	@Test
-	public void test() throws IOException {
+	public void createDatabaseAndInsightFromCSV_test() throws IOException {
 
 		login(page, UserType.NATIVE);
 		HomePageUtils.navigateToHomePage(page);
@@ -60,7 +62,7 @@ public class BIAppTests extends AbstractE2ETest {
 		BISystemAppUtils.closeWelcomePopup(page);
 		BISystemAppUtils.clickOnCatalogOption(page);
 		BISystemAppUtils.clickOnAddDatabaseButton(page);
-		BISystemAppUtils.enterDatabaseName(page, databaseName, timestamp);
+		BISystemAppUtils.enterDatabaseName(page, databaseNameCSV, timestamp);
 		BISystemAppUtils.uploadCSVFile(page);
 		AICorePageUtils.clickOnButton(page, "Next");
 		AICorePageUtils.clickOnButton(page, "Import");
@@ -72,7 +74,7 @@ public class BIAppTests extends AbstractE2ETest {
 		
 		// create an insight from the newly created database
 		BISystemAppUtils.clickOnAppOption(page);
-		BISystemAppUtils.searchDatabaseName(page, databaseName, timestamp);
+		BISystemAppUtils.searchDatabaseName(page, databaseNameCSV, timestamp);
 		BISystemAppUtils.clickOnAddAllOption(page);
 		BISystemAppUtils.clickOnImportButtonOfSelectedColumns(page);
 		BISystemAppUtils.mouseHoverOnDatabaseFrame(page);
@@ -86,7 +88,61 @@ public class BIAppTests extends AbstractE2ETest {
 		
 	}
 	
-	@AfterAll
+	@Test
+	public void createDatabaseAndInsightFromExcel_test() throws IOException {
+		
+		login(page, UserType.NATIVE);
+		HomePageUtils.navigateToHomePage(page);
+		
+		// create a new app
+		MainMenuUtils.openMainMenu(SetupHooks.getPage());
+		MainMenuUtils.clickOnOpenAppLibrary(SetupHooks.getPage());
+		AppPageUtils.clickOnCreateNewAppButton(page);
+		CreateAppPopupUtils.clickOnGetStartedButton(page, "Drag and Drop");
+		CreateAppPopupUtils.enterAppName(page, appName, timestamp);
+		CreateAppPopupUtils.enterAppDescription(page, "Created by automation script");
+		CreateAppPopupUtils.enterTags(page, "Test1, Test2");
+		CreateAppPopupUtils.clickOnCreateButton(page);
+		
+		// add database from csv file
+		HomePageUtils.navigateToHomePage(page);
+		MainMenuUtils.openMainMenu(SetupHooks.getPage());
+		MainMenuUtils.clickOnOpenAppLibrary(SetupHooks.getPage());
+		HomePageUtils.clickOnSystemApp(page);
+		HomePageUtils.clickOnBIApp(page);
+		BISystemAppUtils.closeWelcomePopup(page);
+		BISystemAppUtils.clickOnCatalogOption(page);
+		BISystemAppUtils.clickOnAddDatabaseButton(page);
+		
+		BISystemAppUtils.selectExcelOption(page);
+		BISystemAppUtils.enterDatabaseName(page, databaseNameExcel, timestamp);
+		BISystemAppUtils.uploadExcelFile(page);
+		AICorePageUtils.clickOnButton(page, "Next");
+		AICorePageUtils.clickOnButton(page, "Import");
+		
+		// TODO toast disappears quickly need a better way to validate
+//		String actualDBToastMessage = BISystemAppUtils.verifyDBCreatedToastMessage(page);
+//		assertEquals(expectedDBToastMessage, actualDBToastMessage, "Database creation failed");
+//		logger.info("the success toast is quick skipping for now");
+		
+		// create an insight from the newly created database
+		BISystemAppUtils.clickOnAppOption(page);
+		BISystemAppUtils.searchDatabaseName(page, databaseNameExcel, timestamp);
+		BISystemAppUtils.clickOnAddAllOption(page);
+		BISystemAppUtils.clickOnImportButtonOfSelectedColumns(page);
+		BISystemAppUtils.mouseHoverOnDatabaseFrame(page);
+		BISystemAppUtils.clickOnVisualizeDataOption(page);
+		BISystemAppUtils.clickOnWorkspaceSaveButton(page);
+		BISystemAppUtils.enterInsightName(page, insightName, timestamp);
+		BISystemAppUtils.selectProjectName(page, appName, timestamp);
+		
+		String actualInsightToastMessage = BISystemAppUtils.verifySavedInsightSuccessMsg(page);
+		assertEquals(actualInsightToastMessage, expectedInsightToastMessage, "Insights creation failed");
+				
+	}
+	
+	
+	@AfterEach
 	public static void cleanUp() {
 		CommonUtils.navigateAndDeleteApp(page, appName + " " + timestamp);
 	}
