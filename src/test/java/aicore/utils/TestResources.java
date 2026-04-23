b2a8853656e897b461e7e36022660b78ec54fb57
ericgonzal8 <ericgonzalez8@deloitte.com>
2026-04-23 13:50:24 -0400

    test: Updated TestResources.java with names and file locations of resource zip files

--- src/test/java/aicore/utils/TestResources.java
+++ src/test/java/aicore/utils/TestResources.java
@@ -1,7 +1,33 @@
 package aicore.utils;
 
 public class TestResources {
+	
+	/** DATABASES */
 	public static String TEST_DATABASE_ZIP = "Database/TestDatabase.zip";
+	public static String TEST_DATABASE_NAME = "TestDatabase";
 	public static String DIABETES_CSV = "Database/diabetes.csv";
 
+	/** MODELS */
+	public static String LLAMA3_70B_INSTRUCT_ZIP = "Model/Llama3-70B-Instruct.zip";
+	public static String LLAMA3_70B_INSTRUCT_NAME = "Llama3-70B-Instruct";
+	
+	
+	/** FUNCTIONS */
+	public static String WEATHER_FUNC_ZIP = "Function/weatherFunctionTest.zip";
+	public static String WEATHER_FUNC_NAME = "WeatherFunctionTest";
+	
+	
+	/** STORAGES */
+	public static String LOCAL_MINIO_ZIP = "Storage/Localminio.zip";
+	public static String LOCAL_MINIO_NAME = "localminio";
+	
+	
+	/** VECTORS */
+	public static String TEST_VECTOR_ZIP = "VectorDatabase/TestVector.zip";
+	public static String TEST_VECTOR_NAME = "TestVector";
+	
+	
+	/** GUARDRAILS */
+	public static String GLINER_ZIP = "Guardrail/Gliner.zip";
+	public static String GLINER_NAME = "Gliner";
 }
