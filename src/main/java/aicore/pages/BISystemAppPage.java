9cede23ff2ef4fd62fb93466336625a5df41b542
jachetheboss <77856102+jachetheboss@users.noreply.github.com>
2026-04-24 16:59:41 -0400

    Update BISystemAppPage.java to call methods from BISystemAppUtils

--- src/main/java/aicore/pages/BISystemAppPage.java
+++ src/main/java/aicore/pages/BISystemAppPage.java
@@ -1,13 +1,8 @@
 package aicore.pages;
 
-import java.nio.file.Paths;
-
-import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 
-import aicore.framework.AICoreTestConstants;
-import aicore.framework.ConfigUtils;
 import aicore.utils.AICorePageUtils;
 import aicore.utils.page.app.BISystemAppUtils;
 
@@ -16,35 +11,16 @@ public class BISystemAppPage {
 	private Page page;
 	private String timestamp;
 
-
 	// App option
-	private static final String APP_OPTION_ID = "#home-nav__item--new-insight";
 
 	// insight option
 	private static final String INSIGHTS_OPTION_ID = "#home-nav__item--insight";
-	private static final String SEARCH_DATABASE_TEXTBOX_XPATH = "//input[@id='smss-search__input' and @placeholder='Search...']";
-	private static final String DATABASE_SEARCH_LIST_XPATH = "//span[text()='{DatabaseName}']";
-	private static final String ADD_ALL_BUTTON_XPATH = "//button[@title='Add All']";
-	private static final String IMPORT_ALL_BUTTON_XPATHS = "//button[@class='smss-btn--outline smss-btn']//span[normalize-space(text())='Import']";
-	private static final String DATABASE_FRAME_XPATH = "#pipeline__component__box";
-	private static final String VISUALIZE_THIS_DATA_OPTION_XPATH = "//button[@title='Visualize this data']";
-	private static final String SAVE_WORKSPACE_BUTTON_XPATH = "//button[@title='Save Workspace']";
-	private static final String PROJECT_NAME_DROPDOWN_XPATH = "//div[@class='smss-dropdown__toggle__text']//div[@class='app-list-dropdown__item__text']";
-	private static final String PROJECT_SEARCH_TEXTBOX_XPATH = "//input[@id='smss-search__input' and @placeholder='Search']";
-	private static final String PROJECT_SEARCH_LIST_XPATH = "//div[@class='smss-dropdown__list__option__label']";
-	private static final String INSIGHT_SAVE_BUTTON_XPATH = "//div[@class='smss-action workspace-save__action']//span[normalize-space(text())='Save']";
-	private static final String INSIGHT_SAVE_TOAST_MESSAGE_XPATH = "//div[@class='smss-alert__content smss-alert__content--closable']";
-
+	
 	// catalog option
-	private static final String EXCEL_OPTION_XPATH = "//div[@class='smss-block__text']//span[text()='Excel']";
-	private static final String ENTER_DATABASE_NAME_TEXTBOX_XPATH = "//input[@placeholder='Database Name']";
-	private static final String UPLOAD_FILE_BUTTON_XPATH = "(//input[@type='file'])[2]";
-	private static final String DATABASE_CREATED_TOAST_MESSAGE_XPATH = "//div[@class='smss-alert__content smss-alert__content--closable']";
 	private static final String VISUALIZATION_TYPE_OPTION_XPATH = "//span[text()='{type}']";
 	private static final String X_AXIS_DROPPABLE_AREA_XPATH = "//span[text()='{axis}']//../../..//ul";
 	private static final String FIELD_BUTTON_XPATH = "//div[@title='{fieldName} is a Number']";
 	private static final String TOOLS_OPTION_XPATH = "//span[text()='{optionName}']";
-	private static final String SELECT_STARTING_POINT_TEXT_XPATH = "//span[text()='Select a Starting Point']";
 	private static final String CHART_TABLE_XPATH = "//div[@role='grid']";
 
 	public BISystemAppPage(Page page, String timestamp) {
@@ -60,11 +36,8 @@ public class BISystemAppPage {
 		BISystemAppUtils.closeWelcomePopup(page);
 	}
 
-	public void clickOnAddOption() {
-		page.click(APP_OPTION_ID);
-		Locator startingPointText = page.locator(SELECT_STARTING_POINT_TEXT_XPATH);
-		AICorePageUtils.waitFor(startingPointText);
-		startingPointText.hover();
+	public void clickOnAppOption() {
+		BISystemAppUtils.clickOnAppOption(page);
 	}
 
 	public void clickOnInsightsOption() {
@@ -76,27 +49,23 @@ public class BISystemAppPage {
 	}
 
 	public void clickOnAddDatabaseButton() {
-		page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add Database")).click();
+		BISystemAppUtils.clickOnAddDatabaseButton(page);
 	}
 
 	public void selectExcelOption() {
-		page.click(EXCEL_OPTION_XPATH);
+		BISystemAppUtils.selectExcelOption(page);
 	}
 
 	public void enterDatabaseName(String databaseName) {
-		Locator databaseNameTextbox = page.locator(ENTER_DATABASE_NAME_TEXTBOX_XPATH);
-		AICorePageUtils.waitFor(databaseNameTextbox);
-		databaseNameTextbox.fill(databaseName);
+		BISystemAppUtils.enterDatabaseName(page, databaseName);
 	}
 
 	public void uploadCSVFile() {
-		page.setInputFiles(UPLOAD_FILE_BUTTON_XPATH,
-				Paths.get(ConfigUtils.getValue(AICoreTestConstants.BI_IMPORT_CSV_FILE)));
+		BISystemAppUtils.uploadCSVFile(page);
 	}
 
 	public void uploadExcelFile() {
-		page.setInputFiles(UPLOAD_FILE_BUTTON_XPATH,
-				Paths.get(ConfigUtils.getValue(AICoreTestConstants.BI_IMPORT_EXCEL_FILE)));
+		BISystemAppUtils.uploadExcelFile(page);
 	}
 
 	public void clickOnNextButton() {
@@ -108,53 +77,43 @@ public class BISystemAppPage {
 	}
 
 	public String verifyDBCreatedToastMessage() {
-		Locator toast = page.locator(DATABASE_CREATED_TOAST_MESSAGE_XPATH).first();
-		toast.isVisible();
-		return toast.textContent().trim();
-
+		return BISystemAppUtils.verifyDBCreatedToastMessage(page);
 	}
 
-	public void searchDatabaseName(String createdDatabaseName) {
-		page.fill(SEARCH_DATABASE_TEXTBOX_XPATH, createdDatabaseName);
-		page.click(DATABASE_SEARCH_LIST_XPATH.replace("{DatabaseName}", createdDatabaseName));
+	public void searchDatabaseName(String databaseName) {
+		BISystemAppUtils.searchDatabaseName(page, databaseName);
 	}
 
 	public void clickOnAddAllOption() {
-		page.click(ADD_ALL_BUTTON_XPATH);
+		BISystemAppUtils.clickOnAddAllOption(page);
 	}
 
 	public void clickOnImportButtonOfSelectedColumns() {
-		page.click(IMPORT_ALL_BUTTON_XPATHS);
+		BISystemAppUtils.clickOnImportButtonOfSelectedColumns(page);
 	}
 
 	public void mouseHoverOnDatabaseFrame() {
-		page.hover(DATABASE_FRAME_XPATH);
+		BISystemAppUtils.mouseHoverOnDatabaseFrame(page);
 	}
 
 	public void clickOnVisualizeDataOption() {
-		page.click(VISUALIZE_THIS_DATA_OPTION_XPATH);
-		page.waitForTimeout(300);
+		BISystemAppUtils.clickOnVisualizeDataOption(page);
 	}
 
 	public void clickOnWorkspaceSaveButton() {
-		page.click(SAVE_WORKSPACE_BUTTON_XPATH);
+		BISystemAppUtils.clickOnWorkspaceSaveButton(page);
 	}
 
 	public void enterInsightName(String insightName) {
-		BISystemAppUtils.enterInsightName(page,  insightName + timestamp);
+		BISystemAppUtils.enterInsightName(page, insightName);
 	}
 
 	public void selectProjectName(String projectName) {
-		page.click(PROJECT_NAME_DROPDOWN_XPATH);
-		page.fill(PROJECT_SEARCH_TEXTBOX_XPATH, projectName + " " + timestamp);
-		page.waitForSelector(PROJECT_SEARCH_LIST_XPATH);
-		page.click(PROJECT_SEARCH_LIST_XPATH);
-		page.click(INSIGHT_SAVE_BUTTON_XPATH);
+		BISystemAppUtils.selectProjectName(page, projectName);
 	}
 
 	public String verifySavedInsightSuccessMsg() {
-		Locator toast = page.locator(INSIGHT_SAVE_TOAST_MESSAGE_XPATH).last();
-		return toast.textContent().trim();
+		return BISystemAppUtils.verifySavedInsightSuccessMsg(page);
 	}
 
 	public void clickOnNewProjectButton() {
