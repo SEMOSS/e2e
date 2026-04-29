93a9be35a4cc71b532711967f04bad664f8b3446
Saini <abhissaini@deloitte.com>
2026-04-29 18:46:47 +0530

    feat(setting): Setting folder failing scenario fixes

--- src/test/java/aicore/steps/MemberSettingSteps.java
+++ src/test/java/aicore/steps/MemberSettingSteps.java
@@ -80,7 +80,7 @@ public class MemberSettingSteps {
 	@When("User clicks on search button")
 	public void user_clicks_on_search_button() {
 		settingPage.clickOnSearchButton();
-		settingPage.clickOnSearchBox();
+		// settingPage.clickOnSearchBox();
 	}
 
 	@Then("User searchs for user having username {string}")
