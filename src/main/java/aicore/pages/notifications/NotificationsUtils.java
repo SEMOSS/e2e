78837d05cd8450ec5bcff3c16e04570c4a02b156
BhaktiKanini <bhakti.zurunge@kanini.com>
2026-05-21 19:30:57 +0530

    feat(#1005): Implement the script for accespt the request as author and (#1007)
    validate the notification
    
    Co-authored-by: bhasopanzurunge <bhasopanzurunge@DT-V11APEP1-661.atrame.deloitte.com>

--- src/main/java/aicore/pages/notifications/NotificationsUtils.java
+++ src/main/java/aicore/pages/notifications/NotificationsUtils.java
@@ -4,10 +4,11 @@ import com.microsoft.playwright.Page;
 
 public class NotificationsUtils {
 	public static final String NOTIFICATION_BELL_ICON_XPATH_ = "//button[@aria-label='Notifications']";
-	public static final String USER_ADDED_NOTIFICATION_MESSAGE_FOR_USER_XPATH = "//div[@data-testid='notification-item'][.//span[normalize-space()='{accessProvided}'] and .//span[normalize-space()='{catalogName}'] and .//span[normalize-space()='{accessProvidedByUser}User']] //div[span[contains(text(),'You are added as')]]";
+	public static final String USER_ADDED_NOTIFICATION_MESSAGE_FOR_USER_XPATH = "//div[@data-testid='notification-item'][.//span[normalize-space()='{accessProvided}'] and .//span[normalize-space()='{catalogName}'] and .//span[normalize-space()='{accessProvidedByUser}']] //div[span[contains(text(),'You are added as')]]";
 	public static final String NOTIFICATION_PANE_CLOSE_XPATH = "//h2[text()='Notifications']/parent::div/following-sibling::button[span[text()='Close']]";
-	public static final String USER_ADDED_NOTIFICATION_MESSAGE_FOR_OWNER_XPATH = "//div[@data-testid='notification-item'][.//span[normalize-space()='{userName} User'] and .//span[normalize-space()='{accessProvided}'] and .//span[normalize-space()='{catalogName}']] //div[span[contains(text(),'has been added as')]]";
-	public static final String USER_ADDED_NOTIFICATION_MESSAGE_FOR_OTHER_OWNER_XPATH = "//div[@data-testid='notification-item'][.//span[normalize-space()=\"{userName} User's\"] and .//span[normalize-space()='{accessProvided}'] and .//span[normalize-space()='{catalogName}'] and .//span[normalize-space()='{accessProvidedByUser}User']]//div[span[contains(text(),'has been added as')]]";
+	public static final String USER_ADDED_NOTIFICATION_MESSAGE_FOR_OWNER_XPATH = "//div[@data-testid='notification-item'][.//span[normalize-space()='{accessProvided}'] and .//span[normalize-space()='{catalogName}']] //div[span[contains(text(),'has been added as')]]";
+	public static final String USER_ADDED_NOTIFICATION_MESSAGE_FOR_OTHER_OWNER_XPATH = "//div[@data-testid='notification-item'][.//span[normalize-space()='{accessProvided}'] and .//span[normalize-space()='{catalogName}'] and .//span[normalize-space()='{accessProvidedByUser}']]//div[span[contains(text(),'has been added as')]]";
+	public static final String REQUEST_ACTION_NOTIFICATION_XPATH = "//div[@data-testid='notification-item']//div[@class='text-sm'][.//span[normalize-space()='Your request for'] and .//span[normalize-space()='{accessType}'] and .//span[normalize-space()='permission on'] and .//span[normalize-space()='{catalogName}'] and .//span[normalize-space()='has been approved by'] and .//span[contains(text(),'{userName}')]]";
 
 	public static void clickOnNotificationBellIcon(Page page) {
 		page.locator(NOTIFICATION_BELL_ICON_XPATH_).click();
@@ -23,25 +24,32 @@ public class NotificationsUtils {
 		return actualMessage;
 	}
 
-	public static String validateUserAddedNotificationMessageForOwner(Page page, String userName, String accessProvided,
+	public static String validateUserAddedNotificationMessageForOwner(Page page, String accessProvided,
 			String catalogName) {
-		String actualMessage = page
-				.locator(USER_ADDED_NOTIFICATION_MESSAGE_FOR_OWNER_XPATH.replace("{userName}", userName)
-						.replace("{accessProvided}", accessProvided).replace("{catalogName}", catalogName))
-				.first().textContent().trim();
+		String actualMessage = page.locator(USER_ADDED_NOTIFICATION_MESSAGE_FOR_OWNER_XPATH
+				.replace("{accessProvided}", accessProvided).replace("{catalogName}", catalogName)).first()
+				.textContent().trim();
 
 		return actualMessage;
 	}
 
-	public static String validateUserAddedNotificationMessageForOtherOwner(Page page, String userName, String role,
-			String catalogName, String addedBy) {
-		String actualMessage = page.locator(USER_ADDED_NOTIFICATION_MESSAGE_FOR_OTHER_OWNER_XPATH
-				.replace("{userName}", userName).replace("{accessProvided}", role).replace("{catalogName}", catalogName)
-				.replace("{accessProvidedByUser}", addedBy)).first().textContent().trim();
+	public static String validateUserAddedNotificationMessageForOtherOwner(Page page, String role, String catalogName,
+			String addedBy) {
+		String actualMessage = page
+				.locator(USER_ADDED_NOTIFICATION_MESSAGE_FOR_OTHER_OWNER_XPATH.replace("{accessProvided}", role)
+						.replace("{catalogName}", catalogName).replace("{accessProvidedByUser}", addedBy))
+				.first().textContent().trim();
 		return actualMessage;
 	}
 
 	public static void closeNotificationPane(Page page) {
 		page.locator(NOTIFICATION_PANE_CLOSE_XPATH).click();
+	}
+
+	public static String validateActionPerformOnRequestAccessNotificationMessage(Page page, String accessType,
+			String catalogName, String byUser) {
+		String actualMessage = page.locator(REQUEST_ACTION_NOTIFICATION_XPATH.replace("{accessType}", accessType)
+				.replace("{catalogName}", catalogName).replace("{userName}", byUser)).textContent().trim();
+		return actualMessage;
 	}
 }
