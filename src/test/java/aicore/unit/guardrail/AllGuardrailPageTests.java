826c795624cb773509a3661e0a8cc5e37ef94b43
Clymer, David <dclymer92@hotmail.com>
2026-05-21 08:39:45 -0400

    test: Added junit tests for Guardrail page

--- src/test/java/aicore/unit/guardrail/AllGuardrailPageTests.java
+++ src/test/java/aicore/unit/guardrail/AllGuardrailPageTests.java
@@ -5,30 +5,47 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import com.microsoft.playwright.Page;
+
+import aicore.pages.AddFunctionToCatalogPage;
+import aicore.pages.AddModelPage;
 import aicore.pages.base.EditMetadataPageUtils;
 import aicore.pages.home.MainMenuUtils;
 import aicore.pages.model.EditModelPageUtils;
-import aicore.utils.AbstractE2ETest;
+import aicore.pages.model.SettingsModelPageUtils;
+import aicore.steps.CatalogFilterSteps;
+import aicore.utils.AbstractPlaywrightTestBase;
 import aicore.utils.AddDatabasePageUtils;
+import aicore.utils.AddFunctionPageUtils;
 import aicore.utils.CommonUtils;
 import aicore.utils.GuardrailPageUtils;
 import aicore.utils.GuardrailTestUtils;
 import aicore.utils.TestResourceTrackerHelper;
+import aicore.utils.TestResources;
+import aicore.utils.annotations.PWPage;
+import aicore.utils.annotations.ResourceUploadLock;
 
-public class AllGuardrailPageTests extends AbstractE2ETest {
+public class AllGuardrailPageTests extends AbstractPlaywrightTestBase {
 	private String GUARDRAIL_ID = null;
 	private String GUARDRAIL_NAME = null;
+	private AddModelPage openModelPage;
 
 	
-	@BeforeAll
-	public void setupAddGuardrail() throws IOException {
-		login(page, UserType.NATIVE);
+	@BeforeEach
+	@ResourceUploadLock(TestResources.GLINER_ZIP)
+	public void setupAddGuardrail(@PWPage Page page) throws IOException {
+		loginNativeAdmin(page);
 		
 		// add guardrail
 		String timestamp = CommonUtils.getTimeStampName();
@@ -43,7 +60,6 @@ public class AllGuardrailPageTests extends AbstractE2ETest {
 		EditMetadataPageUtils.enterTagName(page, "embeddings");
 		EditMetadataPageUtils.enterTagName(page, "Test1");
 		EditMetadataPageUtils.enterTagName(page, "Test2");
-		EditMetadataPageUtils.enterTagName(page, "Test3");
 
 		// domains
 		EditMetadataPageUtils.enterDomainName(page, "SAP");
@@ -62,29 +78,24 @@ public class AllGuardrailPageTests extends AbstractE2ETest {
 	}
 	
 	@Test
-	void testCatalogCard() {
-		MainMenuUtils.openMainMenu(page);
-		MainMenuUtils.clickOnGuardrail(page);
-		GuardrailPageUtils.searchGuardrailCatalog(page, GUARDRAIL_NAME);
-		
-		// validate search
-		String databaseNameInCatalog = AddDatabasePageUtils.verifyDatabaseNameInCatalog(page, GUARDRAIL_NAME);
-		boolean databaseNameFlag = databaseNameInCatalog.contains(GUARDRAIL_NAME);
-		Assertions.assertTrue(databaseNameFlag, "Database name is not visible in the database catalog");
+	@ResourceUploadLock(TestResources.GLINER_ZIP)
+	void testCatalogCard(@PWPage Page page) {
+		openGuardrailsAndValidateSearch(page);
 
 		// test id on card
 		boolean isIdVisible = EditModelPageUtils.validateIDisDisplayedOnCatalogCard(page, GUARDRAIL_NAME);
 		Assertions.assertTrue(isIdVisible, "Catalog ID is not visible on the catalog card");
 
 		// validate tags on card
-		String[] tagArray = "embeddings, Test1".split(", ");
-		List<String> actualTagList = EditModelPageUtils.verifyTagNamesDisplayedOnCard(page, "Database");
+		String[] tagArray = "embeddings, Test1, Test2".split(", ");
+		List<String> actualTagList = EditModelPageUtils.verifyTagNamesDisplayedOnCard(page, "GUARDRAIL");
 		List<String> expectedTagList = Arrays.asList(tagArray);
 		Assertions.assertEquals(expectedTagList, actualTagList);
 
 		assertTrue(EditModelPageUtils.isCreatedDateVisibleOnCard(page));
 
 		//TODO make tests to test the behavior of clicking on these icons
+		//Checks if contents of catalog card is correct
 		List<String> icons = List.of("lock", "bookmark", "view logs dashboard", "delete");
 		for (String icon : icons) {
 			boolean isIconVisible = EditModelPageUtils.isIconVisibleOnCatalogCard(page, icon);
@@ -92,12 +103,70 @@ public class AllGuardrailPageTests extends AbstractE2ETest {
 		}
 	}
 	
-	@AfterAll
-	void cleanUp() {
-		login(page, UserType.NATIVE);
+	//test filters
+	@ParameterizedTest(name = "{index} => {0} = {1}")
+	@MethodSource("filterData")
+	@ResourceUploadLock(TestResources.GLINER_ZIP)
+	void testGuardRailFilters(String filterCategory, String filterValue, @PWPage Page page) {
+		openGuardrailsAndValidateSearch(page);
+		
+		List<Map<String, String>> mapList = List
+				.of(Map.of("FILTER_CATEGORY", filterCategory, "FILTER_VALUE", filterValue));
+
+		CatalogFilterSteps.validateCatalogFilters(GUARDRAIL_NAME, "FILTER_CATEGORY", "FILTER_VALUE", mapList,
+				page);
+		
+	}
+	
+	//test privacy check
+	@Test
+	void testGuardRailStatus(@PWPage Page page) {
+		openGuardrailsAndValidateSearch(page);
+		EditModelPageUtils.mouseHoverOnEngineAccessStatusIcon(page);
+		String actualStatus = EditModelPageUtils.getEngineAccessStatusTooltipText(page, "Private");
+		Assertions.assertEquals("Private", actualStatus, "Incorrect status");
+		GuardrailPageUtils.selectTheGuardrailCatalog(page, GUARDRAIL_NAME);
+		AddFunctionPageUtils.clickOnAccessControl(page);
+		SettingsModelPageUtils.clickOnMakeCatalogPublicButton(page,"Guardrail");
+		MainMenuUtils.openMainMenu(page);
+		MainMenuUtils.clickOnGuardrail(page);
+		GuardrailPageUtils.searchGuardrailCatalog(page, GUARDRAIL_NAME);
+		
+		EditModelPageUtils.mouseHoverOnEngineAccessStatusIcon(page);
+		actualStatus = EditModelPageUtils.getEngineAccessStatusTooltipText(page,"Global");
+		Assertions.assertEquals("Global", actualStatus, "Incorrect status");
+		
+		
+	}
+	
+	
+	@AfterEach
+	@ResourceUploadLock(TestResources.GLINER_ZIP )
+	void cleanUp(@PWPage Page page) {
+		loginNativeAdmin(page);
 		boolean deleteDb = CommonUtils.navigateAndDeleteCatalog(page, TestResourceTrackerHelper.CATALOG_TYPE_GUARDRAIL, GUARDRAIL_ID);
 		assertTrue(deleteDb);
+		logout(page);
 	}
+	
+	private void openGuardrailsAndValidateSearch(@PWPage Page page) {
+		MainMenuUtils.openMainMenu(page);
+		MainMenuUtils.clickOnGuardrail(page);
+		GuardrailPageUtils.searchGuardrailCatalog(page, GUARDRAIL_NAME);
+
+		String databaseNameInCatalog = AddDatabasePageUtils.verifyDatabaseNameInCatalog(page, GUARDRAIL_NAME);
+		Assertions.assertTrue(databaseNameInCatalog.contains(GUARDRAIL_NAME),
+				"Database name is not visible in the database catalog");
+	}
+	
+	private static Stream<Arguments> filterData() {
+		return Stream.of(Arguments.of("Tag", "embeddings, Test1"), 
+					Arguments.of("Domain", "SAP, AI"),
+					Arguments.of("Data Classification", "IP, PHI, PII, PUBLIC"), 
+					Arguments.of("Data Restrictions", "IP ALLOWED, PHI ALLOWED, FOUO ALLOWED")
+		);
+	}
+
 
 
 }
