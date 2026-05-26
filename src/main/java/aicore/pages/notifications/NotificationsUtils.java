74af0f6e19f1d2d33f9cbe39c4ce6471e0b27fa2
Saini <abhissaini@deloitte.com>
2026-05-26 12:07:14 +0530

    feat(fix): minor fixes

--- src/main/java/aicore/pages/notifications/NotificationsUtils.java
+++ src/main/java/aicore/pages/notifications/NotificationsUtils.java
@@ -5,6 +5,12 @@ import com.microsoft.playwright.Locator;
 
 public class NotificationsUtils {
 	public static final String NOTIFICATION_BELL_ICON_XPATH_ = "//button[@aria-label='Notifications']";
+	public static final String NOTIFICATION_XPATH = "//div[@data-testid='notification-item']//div[span[contains(text(),'{NotificationMessage}')]]";
+	public static final String NO_NOTIFICATION_MESSAGE_XPATH = "//div//p[text()='No notifications yet.']";
+	public static final String READ_SECTION_XPATH = "//div[span[contains(text(),'{notificationMessage}')]]";
+	public static final String NOTIFICATION_UNREAD_SECTION_XPATH = "//button[text()='Unread']";
+	public static final String NOTIFICATION_READ_SECTION_XPATH = "//button[text()='Read']";
+	public static final String NOTIFICATION_CLEAR_ALL_SECTION_XPATH = "//button[text()='Clear All']";
 	public static final String USER_ADDED_NOTIFICATION_MESSAGE_FOR_USER_XPATH = "//div[@data-testid='notification-item'][.//span[normalize-space()='{accessProvided}'] and .//span[normalize-space()='{catalogName}'] and .//span[normalize-space()='{accessProvidedByUser}']] //div[span[contains(text(),'You are added as')]]";
 	public static final String NOTIFICATION_PANE_CLOSE_XPATH = "//h2[text()='Notifications']/parent::div/following-sibling::button[span[text()='Close']]";
 	public static final String USER_ADDED_NOTIFICATION_MESSAGE_FOR_OWNER_XPATH = "//div[@data-testid='notification-item'][.//span[normalize-space()='{accessProvided}'] and .//span[normalize-space()='{catalogName}']] //div[span[contains(text(),'has been added as')]]";
@@ -49,34 +55,34 @@ public class NotificationsUtils {
 
 	public static void clickOnNotificationMessage(Page page, String NotificationMessage) {
 		Locator Notification = page
-				.locator("//div[@data-testid='notification-item']//div[span[contains(text(),'" + NotificationMessage + "')]]").first();
+				.locator(NOTIFICATION_XPATH.replace("{NotificationMessage}", NotificationMessage)).first();
 		Notification.isVisible();
 		Notification.click();
 	}
 
 	public static void clickOnUnreadTab(Page page) {
-		page.locator("//button[text()='Unread']").click();
+		page.locator(NOTIFICATION_UNREAD_SECTION_XPATH).click();
 	}
 
 	public static void clickOnReadTab(Page page) {
-		page.locator("//button[text()='Read']").click();
+		page.locator(NOTIFICATION_READ_SECTION_XPATH).click();
 	}
 
 	public static void clickOnClearAllButton(Page page) {
-		page.locator("//button[text()='Clear All']").click();
+		page.locator(NOTIFICATION_CLEAR_ALL_SECTION_XPATH).click();
 	}
 
 	public static boolean isNotificationMessagePresentInReadSection(Page page, String notificationMessage) {
 		page.waitForTimeout(1500); // Wait for the notification message to appear in the Read section
-		return page.locator("//div[span[contains(text(),'" + notificationMessage + "')]]").isVisible();
+		return page.locator(READ_SECTION_XPATH.replace("{notificationMessage}", notificationMessage)).isVisible();
 	}
 
 	public static boolean isUnreadSectionBlank(Page page) {
-		return page.locator("///div//p[text()='No notifications yet.']").isVisible();
+		return page.locator(NO_NOTIFICATION_MESSAGE_XPATH).isVisible();
 	}
 
 	public static boolean isReadSectionBlank(Page page) {
-		return page.locator("//div//p[text()='No notifications yet.']").isVisible();
+		return page.locator(NO_NOTIFICATION_MESSAGE_XPATH).isVisible();
 	}
 
 	public static String validateActionPerformOnRequestAccessNotificationMessage(Page page, String accessType,
