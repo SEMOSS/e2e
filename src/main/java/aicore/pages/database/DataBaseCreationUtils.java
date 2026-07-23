fcbb3b12dc73a321955f1f5f4d0a3de62b158ed8
3ashour <ziad.3ashour72@gmail.com>
2026-07-23 17:05:29 +0300

    test: Cleaning DataBaseCreationUtils.java

--- src/main/java/aicore/pages/database/DataBaseCreationUtils.java
+++ src/main/java/aicore/pages/database/DataBaseCreationUtils.java
@@ -1,12 +1,8 @@
 package aicore.pages.database;
-
 import com.microsoft.playwright.Page;
-
 import aicore.pages.home.MainMenuUtils;
 import aicore.utils.CatalogCreationFromZipUtil;
 import aicore.utils.CatlogAccessPageUtility;
-import aicore.utils.CommonUtils;
-import aicore.utils.TestResourceTrackerHelper;
 
 public final class DataBaseCreationUtils {
 
@@ -14,48 +10,18 @@ public final class DataBaseCreationUtils {
     }
     public static String createTestDatabase(Page page) {
 
-        System.out.println("1. Opening Database page...");
         MainMenuUtils.openMainMenu(page);
         MainMenuUtils.clickOnOpenDatabase(page);
 
-        System.out.println("2. Deleting existing database...");
-
-        boolean deleted = CommonUtils.navigateAndDeleteCatalog(
-                page,
-                TestResourceTrackerHelper.CATALOG_TYPE_DATABASE,
-                "TestDatabase");
-
-        System.out.println("Database deleted: " + deleted);
-
-        System.out.println("3. Clicking Add Database...");
         AddDatabaseFormUtils.clickAddDatabaseButton(page);
-
-        System.out.println("4. Uploading ZIP...");
         CatalogCreationFromZipUtil.clickOnFileUploadIcon(page);
-
         String uploadedFileName =
                 CatalogCreationFromZipUtil.uploadFile(page, "Database/TestDatabase.zip");
 
-        System.out.println("Uploaded: " + uploadedFileName);
-
-        System.out.println("5. Clicking Upload...");
         CatalogCreationFromZipUtil.clickOnUploadButton(page, "Upload");
         page.waitForURL(url -> !url.contains("/engine/database/new"));
-        System.out.println("After upload URL: " + page.url());
-        System.out.println(page.locator("body").innerText());
-
-        System.out.println("6. Copying Catalog ID...");
-        System.out.println(page.url());
-
         page.waitForTimeout(5000);
-
-        System.out.println(page.content());
-		System.out.println(page.url());
-		System.out.println(page.title());
         CatlogAccessPageUtility.getCatalogAndCopyId(page);
-
-        System.out.println("Done.");
-
         return uploadedFileName;
     }
 }
