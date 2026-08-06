89bc5760de8cb48015bcd88a9e645092264b90ea
jachetheboss <jamesshen010203@gmail.com>
2026-08-06 09:45:49 -0400

    Update DeleteDiabetesRecordTemplate.java

--- src/test/java/aicore/unit/template/DeleteDiabetesRecordTemplate.java
+++ src/test/java/aicore/unit/template/DeleteDiabetesRecordTemplate.java
@@ -1,13 +1,17 @@
 package aicore.unit.template;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import com.microsoft.playwright.Page;
-import aicore.pages.database.DataBaseCreationUtils;
+
+import aicore.pages.database.DatabaseCreationUtils;
 import aicore.utils.AbstractPlaywrightTestBase;
 import aicore.utils.AddDatabasePageUtils;
 import aicore.utils.CommonUtils;
+import aicore.utils.TestResourceTrackerHelper;
 import aicore.utils.annotations.PWPage;
 import aicore.utils.page.app.AppTemplatePageUtils;
 import aicore.utils.page.app.CreateAppPopupUtils;
@@ -16,24 +20,35 @@ import aicore.utils.page.app.TemplateCreationUtils;
 
 public class DeleteDiabetesRecordTemplate extends AbstractPlaywrightTestBase{
 	
+	String appName = "Default Name Test App";
 	
-	String timestamp = CommonUtils.getTimeStampName();
-	String appName = "Test app " + timestamp;	
+	private static final String TEMPLATE_NAME = "Delete Diabetes Record";
+	private static final String DATABASE_NAME = "TestDatabase";
+	private static final String QUERY_NAME = "on-page-load";
+	private static final String FILE_NAME = "TestDatabase.zip";
 	
 	@BeforeEach
 	void setup(@PWPage Page page) {
 		loginNativeAdmin(page);
-		String uploaded = DataBaseCreationUtils.createTestDatabase(page);
+		String uploaded = DatabaseCreationUtils.createTestDatabase(page);
 		Assertions.assertEquals(
-			    "TestDatabase.zip",
+			    FILE_NAME,
 			    uploaded,
 			    "Database ZIP wasn't uploaded correctly.");
 		AddDatabasePageUtils.clickOnMetadataTab(page);
+		
+		appName = TemplateCreationUtils.createAppFromTemplate(page, TEMPLATE_NAME);
+		verifyAppCreated(page);
 
 	}	
 	@AfterEach
 	void tearDown(@PWPage Page page) {
 		CommonUtils.navigateAndDeleteApp(page, appName);
+		CommonUtils.navigateAndDeleteCatalog(
+		        page,
+		        TestResourceTrackerHelper.CATALOG_TYPE_DATABASE,
+		        DATABASE_NAME
+		    );
 	    logout(page);
 	}
 	
@@ -44,31 +59,26 @@ public class DeleteDiabetesRecordTemplate extends AbstractPlaywrightTestBase{
 	
 	private void runNotebook(Page page) {
 	    NotebookPageUtils.clickOnNotebooksOption(page);
-	    NotebookPageUtils.clickOnQueryName(page, QUERY_NAME);	    
+	    NotebookPageUtils.clickOnQueryName(page, QUERY_NAME);
 	    NotebookPageUtils.selectDatabaseType(page, DATABASE_NAME);
 	    NotebookPageUtils.clickOnRunCellButtonDatabase(page);
 	    NotebookPageUtils.checkDatabaseOutput(page);
 	}
 	
-	private static final String TEMPLATE_NAME = "Delete Diabetes Record";
-	private static final String DATABASE_NAME = "TestDatabase";
-	private static final String QUERY_NAME = "on-page-load";
 	
 	@Test
 	public void DeleteDiabetesRecordTemplate_test (@PWPage Page page) {
 		
-		TemplateCreationUtils.createAppFromTemplate(page, TEMPLATE_NAME);
-		verifyAppCreated(page);		
 		runNotebook(page);	
 	}
 
 	
+
 	@Test
 	public void DeleteDiabetesRecordTemplateExist_test (@PWPage Page page) {
 		
-		appName = TemplateCreationUtils.createAppFromTemplate(page, "Delete Diabetes Record");
-		verifyAppCreated(page);		
 		AppTemplatePageUtils.clickPreviewButton(page);
+		AppTemplatePageUtils.clickSelectUniqueIDDropdownButton(page);
 		NotebookPageUtils.selectValueFromDropdown(page, "4", "Select Unique ID");
 		NotebookPageUtils.clickOnRecordButton(page, "Delete Record");
 		NotebookPageUtils.checkSuccessMessage(page, "true");
