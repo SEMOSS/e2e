db44250c2937a616a3ab0d9dca97b99cfe5f8d44
sarahzara <sarahleeomara@gmail.com>
2026-08-20 15:15:17 +0300

    fix : refactor CatalogPageUtils.java .
    - added waits and logging .

--- src/main/java/aicore/utils/CatalogPageUtils.java
+++ src/main/java/aicore/utils/CatalogPageUtils.java
@@ -2,16 +2,34 @@ package aicore.utils;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import io.qameta.allure.Step;
 
-/**
- * Every Catalog shares same behavior need to move similar utils here
- */
+
 public class CatalogPageUtils {
-
-	public static void clickOnMetadataTab(Page page) {
-		Locator locator = page.getByTestId("engineLayout-Metadata-tab");
-		AICorePageUtils.waitFor(locator);
-		locator.isVisible();
-		locator.click();
-	}
+    
+    private static final Logger logger = LogManager.getLogger(CatalogPageUtils.class);
+    
+    private static final String METADATA_TAB_DATA_TESTID = "engineLayout-Metadata-tab";
+    
+    @Step("Click on Metadata tab")
+    public static void clickOnMetadataTab(Page page) {
+        logger.info("Clicking on Metadata tab");
+        Locator locator = page.getByTestId(METADATA_TAB_DATA_TESTID);
+        AICorePageUtils.waitFor(locator);
+        locator.isVisible();
+        locator.click();
+        logger.info("Successfully clicked on Metadata tab");
+    }
+    
+    @Step("Check if Metadata tab is visible")
+    public static boolean isMetadataTabVisible(Page page) {
+        logger.info("Checking if Metadata tab is visible");
+        Locator locator = page.getByTestId(METADATA_TAB_DATA_TESTID);
+        boolean isVisible = locator.isVisible();
+        return isVisible;
+    }
+    
 }
+
