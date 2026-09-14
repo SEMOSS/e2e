ad5301a7c4a8044d38f26db8eff4a638c9cdc771
3ashour <ziad.3ashour72@gmail.com>
2026-09-14 14:08:11 +0300

    test: Convert ModelCatalogReadOnlyPermission.java

--- src/test/java/aicore/unit/model/permissions/ModelCatalogReadOnlyPermission.java
+++ src/test/java/aicore/unit/model/permissions/ModelCatalogReadOnlyPermission.java
@@ -3,18 +3,19 @@ package aicore.unit.model.permissions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.WaitForSelectorState;
+
 import aicore.base.GenericSetupUtils;
-import aicore.hooks.SetupHooks;
 import aicore.pages.home.HomePageUtils;
 import aicore.pages.home.MainMenuUtils;
 import aicore.pages.model.AddModelFormUtils;
 import aicore.pages.model.EditModelPageUtils;
 import aicore.pages.model.SettingsModelPageUtils;
-import aicore.pages.model.settings.ModelAccessSettingsUtils;
 import aicore.utils.AbstractPlaywrightTestBase;
 import aicore.utils.CatlogAccessPageUtility;
 import aicore.utils.CommonUtils;
@@ -22,10 +23,9 @@ import aicore.utils.TestResourceTrackerHelper;
 import aicore.utils.annotations.PWPage;
 import aicore.utils.page.model.ModelPageUtils;
 
-public class ModelCatalogEditorPermissions extends AbstractPlaywrightTestBase{
+public class ModelCatalogReadOnlyPermission  extends AbstractPlaywrightTestBase {
 	
 	private String modelCatalogName;
-
 
 	@BeforeEach
 	void setup(@PWPage Page page) {
@@ -41,18 +41,18 @@ public class ModelCatalogEditorPermissions extends AbstractPlaywrightTestBase{
 		AddModelFormUtils.clickOnCreateModelButton(page);
 		ModelPageUtils.verifyModelTitle(page, modelCatalogName + "");
 		clickOnCopyCatalogId(page);
+		verifyModelTitle(page, modelCatalogName);
 		SettingsModelPageUtils.clickOnAccessControl(page);
 		SettingsModelPageUtils.clickOnAddMembersButton(page);
-		addMember(page, "Editor");
+		addMember(page, "Read");
 	    logout(page);
-	    loginEditor(page);
+	    loginReadOnly(page);
 		openModelPage(page);
 		EditModelPageUtils.searchModelCatalog(page, modelCatalogName);
 		EditModelPageUtils.selectModelFromSearchOptions(page, modelCatalogName);
 		verifyModelTitle(page, modelCatalogName);
-		CatlogAccessPageUtility.canViewOverview(page);  
-
-	}
+		CatlogAccessPageUtility.canViewOverview(page);
+		}
 	
 	@AfterEach
 	void tearDown(@PWPage Page page) {
@@ -61,13 +61,6 @@ public class ModelCatalogEditorPermissions extends AbstractPlaywrightTestBase{
 	}
 	
 	
-    private void clickOnCopyCatalogId(Page page) {
-        Locator copyCatalogIdButton = page.getByTestId("engineHeader-copy-Model-id-btn");
-        copyCatalogIdButton.waitFor(
-                new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
-        copyCatalogIdButton.click();
-    }
-
     private void addMember(Page page, String role) {
         try {
             SettingsModelPageUtils.addMember(page, role, GenericSetupUtils.useDocker());
@@ -76,7 +69,30 @@ public class ModelCatalogEditorPermissions extends AbstractPlaywrightTestBase{
         }
     }
 
+	private void openModelPage(Page page) {
+
+	    HomePageUtils.navigateToHomePage(page);
+	    MainMenuUtils.openMainMenu(page);
+	    MainMenuUtils.clickOnOpenModel(page);
+	}
 	
+	
+	private void clickAddModelButton(Page page) {
+	    Locator addModelButton = page.getByTestId("engineIndex-add-Model-btn");
+	    addModelButton.waitFor(
+	            new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
+	    addModelButton.click();
+	}
+
+	
+
+    private void clickOnCopyCatalogId(Page page) {
+        Locator copyCatalogIdButton = page.getByTestId("engineHeader-copy-Model-id-btn");
+        copyCatalogIdButton.waitFor(
+                new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
+        copyCatalogIdButton.click();
+    }
+
 	private void verifyModelTitle(Page page, String modelTitle) {
 	    String actualModelTitle = ModelPageUtils.verifyModelTitle(page, modelTitle);
 	    String expectedModelTitle = ModelPageUtils.getExpectedCatalogTitle(modelTitle);
@@ -86,100 +102,66 @@ public class ModelCatalogEditorPermissions extends AbstractPlaywrightTestBase{
 	            actualModelTitle,
 	            "Model title does not match");
 	}
-    
-	private void clickAddModelButton(Page page) {
-	    Locator addModelButton = page.getByTestId("engineIndex-add-Model-btn");
-	    addModelButton.waitFor(
-	            new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
-	    addModelButton.click();
-	}
-
-	
-	private void openModelPage(Page page) {
-
-	    HomePageUtils.navigateToHomePage(page);
-	    MainMenuUtils.openMainMenu(page);
-	    MainMenuUtils.clickOnOpenModel(page);
-	}
 	
 
 	@Test
-    public void ModelCatalogEditorViewUsage_test(@PWPage Page page){
+    public void ModelCatalogReadViewUsage_test(@PWPage Page page){
 		
 		CatlogAccessPageUtility.canViewUsage(page);
 		logout(page);
 		loginNativeAdmin(page);	
 		
 	}
+	
 
 	@Test
-    public void ModelCatalogEditorSmsDeltails_test(@PWPage Page page){
+    public void ModelCatalogReadSmsDetails_test(@PWPage Page page){
 		
 		CatlogAccessPageUtility.canViewSMSSDetails(page);
-	    logout(page);
+		logout(page);
 		loginNativeAdmin(page);	
 		
 	}
+	
+
+	@Test
+    public void ModelCatalogReadEditSms_test(@PWPage Page page){
 		
+		CatlogAccessPageUtility.canViewSMSSDetails(page);
+		CatlogAccessPageUtility.canViewEditSMSS(page);
+		logout(page);
+		loginNativeAdmin(page);	
+		
+	}
 	
 	@Test
-    public void ModelCatalogEditorAccessControl_test(@PWPage Page page){
+    public void ModelCatalogReadAccessControl_test(@PWPage Page page){
 		
 		CatlogAccessPageUtility.canViewAccessControl(page);
-	    logout(page);
-		loginNativeAdmin(page);	
-		
-	}
-	
-
-	@Test
-    public void ModelCatalogEditorMemberSetting_test(@PWPage Page page){
-		
-		SettingsModelPageUtils.clickOnAccessControl(page);
-		SettingsModelPageUtils.isAddMemberButtonVisible(page);
-	    logout(page);
-		loginNativeAdmin(page);	
-	}
-	
-
-	@Test
-    public void ModelCatalogEditorAddRead_test(@PWPage Page page){
-		
-		SettingsModelPageUtils.clickOnAccessControl(page);
-		SettingsModelPageUtils.clickOnAddMembersButton(page);
-		addMember(page, "Read");
-		openModelPage(page);
-		EditModelPageUtils.searchModelCatalog(page, modelCatalogName);
-		EditModelPageUtils.selectModelFromSearchOptions(page, modelCatalogName);
-	    logout(page);
-		loginNativeAdmin(page);
-	}
-	
-
-	@Test
-    public void ModelCatalogEditorDeleteReadMember_test(@PWPage Page page){
-		
-		SettingsModelPageUtils.clickOnAccessControl(page);
-		SettingsModelPageUtils.clickOnAddMembersButton(page);
-		addMember(page, "Read");
-		CatlogAccessPageUtility.searchUserBasedOnRole(page, "Read");
-		SettingsModelPageUtils.deleteAddedMember(page, "Read");
-	    logout(page);
-		loginNativeAdmin(page);
-	}
-	
-
-	@Test
-    public void ModelCatalogEditorDeleteModelAsAuthor_test(@PWPage Page page){
-		openModelPage(page);
 		logout(page);
-		loginAuthor(page);
-		openModelPage(page);
-		EditModelPageUtils.searchModelCatalog(page, modelCatalogName);
-		EditModelPageUtils.selectModelFromSearchOptions(page, modelCatalogName);
-		SettingsModelPageUtils.clickOnAccessControl(page);
-		ModelAccessSettingsUtils.clickOnDeleteButton(SetupHooks.getPage());
+		loginNativeAdmin(page);	
 	}
 		
+
+	@Test
+    public void ModelCatalogReadMemeberSetting_test(@PWPage Page page){
 		
+		CatlogAccessPageUtility.canViewAccessControl(page);
+		SettingsModelPageUtils.isAddMemberButtonVisible(page);
+		logout(page);
+		loginNativeAdmin(page);	
+	}
+	
+
+
+	@Test
+    public void ModelCatalogReadDeleteModel_test(@PWPage Page page){
+		
+		CatlogAccessPageUtility.canViewAccessControl(page);
+		logout(page);
+		loginNativeAdmin(page);	
+
+	}
+		
+
 }
